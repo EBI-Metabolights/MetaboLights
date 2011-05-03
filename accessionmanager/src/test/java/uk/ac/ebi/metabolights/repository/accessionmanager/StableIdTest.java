7149d0ec452cef7011d1720fafda51f4c99e2569
pconesa <p.conesa.mingo@gmail.com>
2011-05-03 15:11:17 +0000

    Tests for accession manager
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@121 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

---
+++ accessionmanager/src/test/java/uk/ac/ebi/metabolights/repository/accessionmanager/StableIdTest.java
@@ -0,0 +1,28 @@
+package uk.ac.ebi.metabolights.repository.accessionmanager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StableIdTest {
+
+	final Integer ID=3;
+	final String PREFIX="HOLA";
+	
+	@Test
+	public void testId() {
+		StableId stableId = new StableId();
+		
+		stableId.setSeq(ID);
+		assertEquals(ID, stableId.getSeq());
+		
+	}
+
+	@Test
+	public void testPrefix() {
+		StableId stableId = new StableId();
+		
+		stableId.setPrefix(PREFIX);
+		assertEquals(PREFIX, stableId.getPrefix());
+	}
+}
