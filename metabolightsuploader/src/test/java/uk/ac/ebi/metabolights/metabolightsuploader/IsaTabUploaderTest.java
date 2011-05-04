cdd381aed714c20180080e9bd5c6955c4ec5ee5f
pconesa <p.conesa.mingo@gmail.com>
2011-05-04 11:47:20 +0000

    Comment refactor uplad methods. Cancel uploadWithCommandLine test
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@137 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolightsuploader/src/test/java/uk/ac/ebi/metabolights/metabolightsuploader/IsaTabUploaderTest.java
+++ metabolightsuploader/src/test/java/uk/ac/ebi/metabolights/metabolightsuploader/IsaTabUploaderTest.java
@@ -39,6 +39,23 @@ public class IsaTabUploaderTest {
 		assertEquals("loo",itu.getOwner());
 		
 	}
+	//Do not test this as command line tools is not recommended to use@Test
+	public void testUploadCommandLine() throws ConfigurationException, IsaTabIdReplacerException, IOException {
+		final String ISA_ARCHIVE = "src/test/resources/inputfiles/BII-I-1.zip";
+		final String UNZIP_FOLDER = "src/test/resources/outputfiles/testUpload/";
+		
+		//Instantiate with parameters
+		//TODO assuming conesa is a user in the database already. Ideally test should create the user if it doesn't exist.
+		IsaTabUploader itu = new IsaTabUploader(ISA_ARCHIVE, UNZIP_FOLDER, "conesa");
+		
+		//Load file...
+		itu.UploadWithCommandLine();
+		
+		//If there is no error we assume every thing went well
+		assertTrue("testUpload successful", true);
+		
+		
+	}
 	@Test
 	public void testUpload() throws ConfigurationException, IsaTabIdReplacerException, IOException {
 		final String ISA_ARCHIVE = "src/test/resources/inputfiles/BII-I-1.zip";
@@ -50,23 +67,6 @@ public class IsaTabUploaderTest {
 		
 		//Load file...
 		itu.Upload();
-		
-		//If there is no error we assume every thing went well
-		assertTrue("testUpload successful", true);
-		
-		
-	}
-	@Test
-	public void testUpload2() throws ConfigurationException, IsaTabIdReplacerException, IOException {
-		final String ISA_ARCHIVE = "src/test/resources/inputfiles/BII-I-1.zip";
-		final String UNZIP_FOLDER = "src/test/resources/outputfiles/testUpload/";
-		
-		//Instantiate with parameters
-		//TODO assuming conesa is a user in the database already. Ideally test should create the user if it doesn't exist.
-		IsaTabUploader itu = new IsaTabUploader(ISA_ARCHIVE, UNZIP_FOLDER, "conesa");
-		
-		//Load file...
-		itu.Upload2();
 		
 		//If there is no error we assume every thing went well
 		assertTrue("testUpload successful", true);
