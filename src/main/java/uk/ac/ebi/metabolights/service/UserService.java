76b15b08c17c7d6e220e46a0328fec5965fd3454
khaug <haug.kenneth@gmail.com>
2011-05-05 12:09:58 +0000

    Moved to metabolights-webapps
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@165 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- src/main/java/uk/ac/ebi/metabolights/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ebi.metabolights.service;
-
-import uk.ac.ebi.metabolights.authenticate.MetabolightsUser;
-
-public interface UserService {
-	public MetabolightsUser lookupByName(String username);
-}
