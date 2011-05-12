a4d3cc3af1c11765f4852a475fad0c3cc642b18c
samydeghou <samy.deghou@gmail.com>
2011-05-12 10:50:11 +0000

    peak explicit and modified
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@207 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- specview/src/main/javascript/specview/specview/model/peak.js
+++ specview/src/main/javascript/specview/specview/model/peak.js
@@ -5,30 +5,91 @@ goog.require('goog.math.Coordinate');
  * Constructeur of the peak
  */
 specview.model.Peak=function(opt_xValue,opt_intensity,opt_peakId,opt_atomRef,opt_multiplicity,opt_molRefs,opt_Xunit,opt_Yunit,opt_isVisible){
+
+	/**
+	 * xValue is the value of the shift of the peak as found in the cml file. Unit is in ppm
+	 */
 	this.xValue=opt_xValue;
+	
+	/**
+	 * intensity is the intensity of the peak.
+	 * For cml files holding Mass Spectrometry experiment, the intensity is an information provided.
+	 * For cml files holding NMR experiment two possible cases:
+	 * 		(i) 	The NMR experiment is a C-NMR experiment: in that case, every peak has the same height and thence,
+	 * 				the height is not a an information provided.
+	 * 		(ii)	The NMR experiment is a H-NMR experiment: in that case, not every peak has the same height. In that
+	 * 				case, two possible situations:
+	 * 					(i)  Sometimes, the information on the height is available in the file.
+	 * 					(ii) If not, the height information should be computed on the fly.
+	 */
 	this.intensity=opt_intensity;
+	
+	/**
+	 * peakId is a peak identifier that is provided within the cml file
+	 */
 	this.peakId=opt_peakId;
+	
+	/**
+	 * If the cml file holds NMR experiment data, and if it is an annotated file, then the peak are linked to one or
+	 * multiple atoms. atomref is an atom identifier or an array of atom identifier found in the file.
+	 */
 	this.atomRef=opt_atomRef;// Should be an array
+	
+	/**
+	 * If the cml file holds NMR experiment data, the peak can have different shape: singulet, doublet, triplet etc.
+	 * This information is provided in the file.
+	 */
 	this.multiplicity=opt_multiplicity;
+	
+	/**
+	 * coord are the coordinate of the peak based on its xValue
+	 */
 	this.coord=new goog.math.Coordinate(this.xValue,-0.16);
+	
+	/**
+	 * 
+	 */
 	this.atomMap=new Array();
 	this.atomMap[this.peakId]=this.atomRef;// Hence it is an array of array. Ex : ["p1":["a1","a2",...,"ai"]]
+	
+	/**
+	 * xPixel and yPixel the the transformed coordinate of the peak, the "real" coordinate in pixel unit to which it is displayed
+	 * xPixel and yPixel are the "starting" coordinates of the peak, its origine
+	 */
 	this.xPixel=0;
 	this.yPixel=0;
 	
+	/**
+	 * xTpixel and yTpixel the the transformed coordinate of the peak, the "real" coordinate in pixel unit to which it is displayed
+	 * xTpixel and yTpixel are the "ending" coordinates of the peak.
+	 * xTpixel==xPixel
+	 * Math.abs(yTpixel-yPixel)=height of the peak
+	 */	
 	this.xTpixel=0;
 	this.yTpixel=0;
 	
+	/**
+	 * The unit of the shift and of the intensity.
+	 * These are displayed on the axis
+	 */
 	this.peakXunit=opt_Xunit;
 	this.peakYunit=opt_Yunit;
 
 	this.pixelCoord=null;
 	
+	/**
+	 * isVisible is a boolean.
+	 * If the xPixel value is inside the box of the spectrum (mainSpecBox of the nmrdata object) isVisble = true and 
+	 * the the peak is displayed to the screen. If isVIsible=false, the coordinate of the peak are outside the box 
+	 * (useful when zooming for instance) and the peak is not displayed.
+	 */
 	this.isVisible=opt_isVisible;
 	
-	/*TODO Consider the case of MS spectras
-		this.arrayOfSecondaryMolecules
-	*/
+	/**
+	 * If the file holds Mass Spec experiment, and if the file is annotated, then some peaks are related to one or many
+	 * molecules.
+	 * arrayOfSecondary molecules contains the molecule identifiers that refers to the molecules.
+	 */
 	this.arrayOfSecondaryMolecules=opt_molRefs;//normally just one molecule
 	
 	
