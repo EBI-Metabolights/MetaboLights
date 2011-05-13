59229350bcf1291d81d1ed601177629100a00042
pconesa <p.conesa.mingo@gmail.com>
2011-05-13 16:18:51 +0000

    Allow specifying the status (public or private)
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@235 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolightsuploader/src/main/java/uk/ac/ebi/metabolights/metabolightsuploader/IsaTabUploader.java
+++ metabolightsuploader/src/main/java/uk/ac/ebi/metabolights/metabolightsuploader/IsaTabUploader.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 
 import org.isatools.isatab.manager.SimpleManager;
 
+import uk.ac.ebi.bioinvindex.model.VisibilityStatus;
+
 
 
 public class IsaTabUploader {
@@ -19,6 +21,7 @@ public class IsaTabUploader {
 	//Use IsaTabReplacerId
 	private IsaTabIdReplacer itir = new IsaTabIdReplacer();
 	private String owner = "";
+	private VisibilityStatus status = VisibilityStatus.PUBLIC;
 	
 	public IsaTabUploader(){
 	}
@@ -27,6 +30,12 @@ public class IsaTabUploader {
 		itir.setIsaTabArchive(isatabfile);
 		itir.setUnzipFolder(unzipfolder);
 		this.owner = owner;
+	}
+	public IsaTabUploader(String isatabfile, String unzipfolder, String owner, VisibilityStatus status){
+		
+		this(isatabfile,unzipfolder,owner);
+		this.status = status;
+		
 	}
 	
 	public void setIsaTabFile(String isatabfile){
@@ -47,37 +56,12 @@ public class IsaTabUploader {
 	public String getOwner(){
 		return owner;
 	}
-	
-//	/**
-//	 * Upload an isa tab file using members for the file, unzip folder and owner. It also assign the owner to the file
-//	 * @throws ConfigurationException
-//	 * @throws IsaTabIdReplacerException
-//	 * @throws IOException
-//	 */
-//	public void UploadWithCommandLine() throws ConfigurationException, IsaTabIdReplacerException, IOException{
-//
-//		//Replace the id
-//		itir.Execute();
-//
-//		//Avoid System Exit. This is done at the end of PersistenceShellCommand.main 
-//		forbidSystemExitCall();
-//		
-//		try{
-//			//Invoke persistence command
-//			//how to get feedback, connect to the log. or pass the log file as parameter.
-//			PersistenceShellCommand.main(new String[]{itir.getUnzipFolder()});
-//			
-//		}catch(ExitTrappedException e){
-//			
-//		}finally{
-//			enableSystemExitCall();
-//		}
-//			
-//	
-//		//Assign the user to the experiment
-//		assignOwners(getOwner(), itir.getAccessionNumberList());
-//			
-//	}
+	public void setStatus(VisibilityStatus status){
+		this.status = status;
+	}
+	public VisibilityStatus getStatus(){
+		return status;
+	}
 	/**
 	 * Upload an experiment (Isa Tab zip file) into BII.
 	 * @throws ConfigurationException
@@ -96,87 +80,13 @@ public class IsaTabUploader {
 		SimpleManager sm = new SimpleManager();
 		
 		//Load the isatab file
-		//TODO: When uploading an id that already exists, it does not throw any exception, it only log it!!!
-		sm.loadISAtab(getUnzipFolder(), owner);
+		//TODO: When uploading an id that already exists, it does not throw any exception, it only logs it!!!
+		sm.loadISATab(getUnzipFolder(), owner,status);
 		
 		//Return the new accession numbers
 		return itir.getAccessionNumberList();
 		
 	}
-//	/**
-//	 * Assign all the owners to all the studies, to unassign use a - sign prefixing the user
-//	 * @param owners: String with owners separated by commas.[+|-]login1,login2,... if +|- is specified the users will
-//     *             be added to / removed from the study, otherwise this will be the exact list of study's users
-//	 * @param studies: String with studies separated by spaces
-//	 */
-//	public void assignOwners (String owners, String studies){
-//		//We are looking for something like this:
-//		//--study-owners BII-S-1=-zakmck,rose --study-owners BII-S-2=+zakmck,rose --public BII-S-1,BII-S-2
-//	
-//		String[] studiesList;
-//		String ownerscommandline = "";
-//		String publiccommandline;
-//		String[] args;
-//		
-//		//Convert strings into arrays
-//		studiesList = studies.split(" ");
-//		
-//		//init the public param
-//		publiccommandline = " --public ";
-//		
-//		//For each study
-//		for (int i=0;i<studiesList.length;i++){
-//			
-//			//Start a study owner param
-//			ownerscommandline = ownerscommandline
-//								+ " --study-owners " 
-//								+ studiesList[i] + "=" + owners;
-//			
-//			//Set the public status...
-//			//This might be a parameter but for now lets go straightforward
-//			publiccommandline = publiccommandline + studiesList[i] + ",";
-//			
-//		}
-//
-//		
-//		//Truncate the last comma of the public command line
-//		publiccommandline = StringUtils.truncate(publiccommandline);
-//		
-//		//Create args array joining both command lines
-//		args = (ownerscommandline + publiccommandline).split(" ");
-//		
-//
-//		//Avoid System Exit. This is done at the end of PermModShellCommand.main 
-//		forbidSystemExitCall();
-//
-//		try{
-//			//Invoke the permission manager command line
-//			PermModShellCommand.main(args);
-//
-//		}catch(ExitTrappedException e){
-//			
-//		}finally{
-//			enableSystemExitCall();
-//		}
-//
-//	}
-//
-//	private static class ExitTrappedException extends SecurityException {}
-//
-//	private static void forbidSystemExitCall() {
-//	  final SecurityManager securityManager = new SecurityManager() {
-//	    public void checkPermission( Permission permission ) {
-//	      if(permission.getName().indexOf("exitVM")==0) {
-//	        throw new ExitTrappedException() ;
-//	      }
-//	    }
-//	  } ;
-//	  System.setSecurityManager( securityManager ) ;
-//	}
-//
-//	private static void enableSystemExitCall() {
-//	  System.setSecurityManager( null ) ;
-//	}
 }
 	
 
