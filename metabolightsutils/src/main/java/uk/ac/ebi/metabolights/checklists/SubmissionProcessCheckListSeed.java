3f2c18cfd2c2c08c2d4a83c2e1d1b487828d9a7f
pconesa <p.conesa.mingo@gmail.com>
2011-06-23 15:55:44 +0000

    Add BII validation Step
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@447 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolightsutils/src/main/java/uk/ac/ebi/metabolights/checklists/SubmissionProcessCheckListSeed.java
+++ metabolightsutils/src/main/java/uk/ac/ebi/metabolights/checklists/SubmissionProcessCheckListSeed.java
@@ -5,9 +5,9 @@ public enum SubmissionProcessCheckListSeed implements ICheckListItemSeed{
 	FILEVALIDATION("2", "File basic validation"),
 	FILEUNZIP("3","File unzip"),
 	CONTENTVALIDATION("4","Experiment data validation"),
-	IDREPLACEMENTS("4","Metabolights Id assignment"),
-	FILEPERSISTANCE("5","File persistance"),
-	SETPERMISSIONS ("6","Permission and visibility set up")	
+	IDREPLACEMENTS("5","Metabolights Id assignment"),
+	FILEPERSISTANCE("6","File persistance"),
+	SETPERMISSIONS ("7","Permission and visibility set up")	
 	;
 	
 	private final String key;
