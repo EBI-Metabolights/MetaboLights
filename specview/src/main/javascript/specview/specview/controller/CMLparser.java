1ec7c9eb6acfa79300e350223a3d5fd7f165b6c4
samydeghou <samy.deghou@gmail.com>
2011-07-05 08:35:53 +0000

    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@498 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

---
+++ specview/src/main/javascript/specview/specview/controller/CMLparser.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.awt.*;
+
+import java.applet.Applet;
+
+public class CMLparser extends Applet{
+
+	  public void init() { }
+	  
+	  // This method is mandatory, but can be empty.(i.e.,have no actual code).
+	  public void stop() { }
+	
+	/*
+	   public String convertXMLFileToString() 
+	   { 
+
+	     try{ 
+	       DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance(); 
+	       InputStream inputStream = new FileInputStream(new File(this.fileName));
+	       org.w3c.dom.Document doc = documentBuilderFactory.newDocumentBuilder().parse(inputStream); 
+	       StringWriter stw = new StringWriter(); 
+	       Transformer serializer = TransformerFactory.newInstance().newTransformer(); 
+	       serializer.transform(new DOMSource(doc), new StreamResult(stw)); 
+	       return stw.toString(); 
+	     } 
+	     catch (Exception e) { 
+	       e.printStackTrace(); 
+	     } 
+	       return null; 
+	   }
+	   */
+	   public void paint(Graphics g)
+	   {
+		    g.drawString("YOur file", 20,10);
+	   }
+	   
+	   public String parse(){
+		   return "/home/epu/test1_frag.cml.1.xml";
+	   }
+		/*
+		   String fileName = "/home/epu/test1_frag.cml.1.xml";
+		     try{ 
+			       DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance(); 
+			       InputStream inputStream = new FileInputStream(new File(fileName));
+			       org.w3c.dom.Document doc = documentBuilderFactory.newDocumentBuilder().parse(inputStream); 
+			       StringWriter stw = new StringWriter(); 
+			       Transformer serializer = TransformerFactory.newInstance().newTransformer(); 
+			       serializer.transform(new DOMSource(doc), new StreamResult(stw)); 
+			       return stw.toString(); 
+			     } 
+			     catch (Exception e) { 
+			       e.printStackTrace(); 
+			     } 
+			       return null; 
+			   }
+		*/
+}
--- specview/src/main/javascript/specview/specview/controller/HelloWorld.java
+++ specview/src/main/javascript/specview/specview/controller/HelloWorld.java
@@ -1,15 +1,20 @@
-
-
-public class HelloWorld extends java.applet.Applet
-{
-   public void paint(java.awt.Graphics g)
-   {
-        g.drawString("Bad ass, mother fucker !",10,10);
-        System.out.println("Hello World!!!!!!!!!!!!!");
-    }
-   
-   public String printMessage(String s){
-	   return s;
-   }
-   
-} 
+import java.applet.Applet;
+import java.awt.*;
+ 
+// Applet code for the "Hello, world!" example.
+// This should be saved in a file named as "HelloWorld.java".
+public class HelloWorld extends Applet {
+  // This method is mandatory, but can be empty (i.e., have no actual code).
+  public void init() { }
+ 
+  // This method is mandatory, but can be empty.(i.e.,have no actual code).
+  public void stop() { }
+ 
+  // Print a message on the screen (x=20, y=10).
+  public void paint(Graphics g) {
+    g.drawString("Putain !", 20,10);
+ 
+  // Draws a circle on the screen (x=40, y=30).
+    g.drawArc(40,30,20,20,0,360);
+  }
+}
