09d376d2c87eb91e66f226a76bf68b72ec3a92a6
pconesa <p.conesa.mingo@gmail.com>
2011-07-05 19:43:58 +0000

    Adapt to the new index
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@502 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/SearchController.java
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/SearchController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import uk.ac.ebi.metabolights.search.LuceneSearchResult;
+import uk.ac.ebi.metabolights.search.LuceneSearchResult.Assay;
 import uk.ac.ebi.metabolights.service.SearchService;
 
 /**
@@ -38,16 +39,18 @@ public class SearchController extends AbstractController{
 	 * @param request
 	 * @return
 	 */
-	@RequestMapping(value = "/search", method = RequestMethod.POST )
+	@RequestMapping(value = "/search", method = {RequestMethod.POST, RequestMethod.GET})
 	public ModelAndView luceneSearch (HttpServletRequest request) {
 
 	    List<LuceneSearchResult> resultSet = new ArrayList<LuceneSearchResult>();
+
 	    List<String> organisms = new ArrayList<String>(); 
 	    List<String> technology = new ArrayList<String>();
-	    List<String> platforms = new ArrayList<String>();
 	    
+	    //HashMap<String, Set> filters = new HashMap();
 	    
-	    String query = request.getParameter("query");
+	    //Get the user free text search
+	    String query = request.getParameter("query")!=null? request.getParameter("query"): "";   
 	    
 		try {
 			logger.info("searching for "+query);
@@ -60,26 +63,14 @@ public class SearchController extends AbstractController{
 				
 				if (result.getOrganism()!=null && !organisms.contains(result.getOrganism())) //Add unique entries to the list
 					organisms.add(result.getOrganism());
+
 				
-				if (result.getTechnologies()!=null && !technology.containsAll(result.getTechnologies())){ 
-					Iterator techIter = result.getTechnologies().iterator();
-					while (techIter.hasNext()){
-						String techs = (String) techIter.next();
-						if (!technology.contains(techs))
-							technology.add(techs); 
-					}
-					
-				}
-				
-				
-				if (result.getPlatforms()!=null && !platforms.containsAll(result.getPlatforms())){ 
-					Iterator platIter = result.getPlatforms().iterator();
-					while (platIter.hasNext()){
-						String plats = (String) platIter.next();
-						if (!platforms.contains(plats))
-							platforms.add(plats); 
-					}
-					
+				//Get the list of technologies..
+				Iterator <Assay> assIter = result.getAssays().iterator();
+				while (assIter.hasNext()){
+					Assay assay = (Assay) assIter.next();
+					if (!technology.contains(assay.getTechnology()))
+						technology.add(assay.getTechnology()); 
 				}
 				
 				
@@ -90,15 +81,13 @@ public class SearchController extends AbstractController{
 		}
     	ModelAndView mav = new ModelAndView("searchResult");
     	mav.addObject("searchResults", resultSet);
-    	mav.addObject("userQuery", request.getParameter("query"));
+    	mav.addObject("userQuery", query);
     	if (!query.isEmpty())
     		mav.addObject("userQueryClean", query.replaceAll("\\*", "")); //TODO, % as well
     	if (organisms.size()>1)
     		mav.addObject("organisms", organisms);
     	if (technology.size()>1)
     		mav.addObject("technology", technology);
-    	if (platforms.size()>1)
-    		mav.addObject("platforms", platforms);
     	
     	
     	return mav;
