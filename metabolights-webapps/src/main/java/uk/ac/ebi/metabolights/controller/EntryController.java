ddb60d69d8d921fc42f34e34bf87415193ef2aee
khaug <haug.kenneth@gmail.com>
2011-08-11 09:53:16 +0000

    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@654 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/EntryController.java
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/EntryController.java
@@ -1,12 +1,24 @@
 package uk.ac.ebi.metabolights.controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeSet;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+
+import uk.ac.ebi.bioinvindex.model.security.User;
 import uk.ac.ebi.bioinvindex.model.term.PropertyValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +26,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import uk.ac.ebi.bioinvindex.model.AssayResult;
 import uk.ac.ebi.bioinvindex.model.Study;
+import uk.ac.ebi.bioinvindex.model.VisibilityStatus;
+import uk.ac.ebi.metabolights.model.MetabolightsUser;
 import uk.ac.ebi.metabolights.service.StudyService;
 
 /**
@@ -27,6 +41,8 @@ public class EntryController extends AbstractController {
 
 	@Autowired
 	private StudyService studyService;
+
+    private @Value("#{appProperties.publicFtpLocation}") String ftpDirectory;
 
 	//(value = "/entry/{metabolightsId}")
 	@RequestMapping(value = "/{metabolightsId}") 
@@ -43,13 +59,74 @@ public class EntryController extends AbstractController {
 				}
 			}
 		}
+
+		String ftpLocation = "";
 		
+		if (study.getStatus().equals(VisibilityStatus.PUBLIC)){  //Serve back public ftp link
+			ftpLocation = ftpDirectory.replaceFirst("/ebi/ftp/","ftp://ftp.ebi.ac.uk/") + "/" + study.getAcc() +".zip";
+		} else if (study.getStatus().equals(VisibilityStatus.PRIVATE))
+		{	// Only for the submitter
+			ftpLocation = "privatefiles/" + study.getAcc();  //Private download, file stream
+		} else {
+			ftpLocation = null;
+		}
+
 		ModelAndView mav = new ModelAndView("entry");
 		mav.addObject("study", study);
 		mav.addObject("organismNames", organismNames);
+        mav.addObject("ftpLocation",ftpLocation);
 
 		return mav;
 	}
+	
+	@RequestMapping(value = "/privatefiles/{file_name}")
+	public void getFile(@PathVariable("file_name") String fileName,
+			HttpServletResponse response) {
+		try {
 
+			Boolean validUser = false;
+			Long userId = new Long(0);
+			
+	 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			if (!auth.getPrincipal().equals(new String("anonymousUser"))){
+				MetabolightsUser principal = (MetabolightsUser) auth.getPrincipal();
+				userId = principal.getUserId();
+			}
+			
+			if (userId>0){
+				Study study = studyService.getBiiStudy(fileName);
+				Collection<User> users = study.getUsers();
+				Iterator<User> iter = users.iterator();
+				while (iter.hasNext()){
+					User user = (User) iter.next();
+					if (user.getId().equals(userId)){
+						validUser = true;
+						break;
+					}
+				}
+				
+			}
+			
+			if (!validUser)
+				throw new RuntimeException("You are not authorised to access this file"); //TODO, hack
+			
+			
+			// get your file as InputStream
+			InputStream is = new FileInputStream(ftpDirectory + "/" + fileName + ".zip");
+
+			// let the browser know it's a zip file
+			response.setContentType("application/zip");
+
+			// copy it to response's OutputStream
+			IOUtils.copy(is, response.getOutputStream());
+			
+			response.flushBuffer();
+			
+		} catch (IOException ex) {
+			logger.info("Error writing file to output stream. Filename was '"+ fileName + "'");
+			throw new RuntimeException("IOError writing file to output stream");
+		}
+
+	}
 
 }
