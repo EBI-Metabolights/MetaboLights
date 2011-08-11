6241044f9a0d964264cee72036ca27451272eb82
khaug <haug.kenneth@gmail.com>
2011-08-11 09:50:19 +0000

    New ftp download links on the entry page
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@652 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/UserAccountController.java
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/UserAccountController.java
@@ -1,7 +1,5 @@
 package uk.ac.ebi.metabolights.controller;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -16,7 +14,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
