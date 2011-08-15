87d9a3ffc6da842c4c4cbf725d4ea3c7eb1e68cb
khaug <haug.kenneth@gmail.com>
2011-08-15 13:29:38 +0000

    Updated pom versions to 0.1.0
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@695 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- rebuildAll.sh
+++ rebuildAll.sh
@@ -2,6 +2,7 @@ mvn clean
 rm metabolights-webapps/src/main/webapp/WEB-INF/lib/*.jar
 mvn dependency:copy-dependencies
 mvn compile
+mvn install -DskipTests=true
 cd metabolights-webapps
 mvn war:inplace war:war
 cd ..
