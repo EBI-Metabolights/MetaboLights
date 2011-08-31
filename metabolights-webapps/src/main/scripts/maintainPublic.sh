ab89092fc4a41234d6baf792d3645ce7ea9efcb9
khaug <haug.kenneth@gmail.com>
2011-08-31 11:34:48 +0000

    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@780 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/scripts/maintainPublic.sh
+++ metabolights-webapps/src/main/scripts/maintainPublic.sh
@@ -1,5 +1,15 @@
 #!/bin/ksh
-
+##########################################################################
+#
+#   File    : maintainPublic.sh
+#   Type    : Korn Shell Script
+#
+#   Purpose : Check the database for recently modified studies and moving
+#             archive files to the public ftp location
+#
+# 20110831  : Ken Haug - Created script
+#
+##########################################################################
 clear
 
 . /homes/mtbl/metabolights/metabolights-webapps/src/main/scripts/logging_functions
@@ -10,23 +20,22 @@ source /homes/oracle/ora11setup.sh
 #################################
 
 EMAILTO=kenneth@ebi.ac.uk
+#MAILTO=`grep mtblAdminEmailAddress ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=` 
 PROPS_FILE=/homes/mtbl/metabolights/metabolights-webapps/src/main/webapp/resources/application.properties
-SQL_DIRECTORY=/homes/mtbl/metabolights/metabolights-webapps/src/main/sql/
 NUM_DAYS=5
 
 #################################
 #  End of Configurable Options  #
 #################################
 
-SHELL_LOG_FILE=/homes/mtbl/maintainPublic.log.`date +"%Y-%m-%d %H:%M:%S"`
-#MAILTO=`grep mtblAdminEmailAddress ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
+SHELL_LOG_FILE=/homes/mtbl/maintainPublic.log.`date +"%Y-%m-%d %H:%M:%S"` 
 DB_CONNECTION=`grep jdbc.username ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
 DB_CONNECTION=$DB_CONNECTION'/'`grep jdbc.password ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
 DB_CONNECTION=$DB_CONNECTION'@'`grep jdbc.databaseurl ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f6 -d:`  
 PUB_FTP=`grep publicFtpLocation ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
 PRIV_FTP=`grep privateFtpLocation ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
-SQL_BASIC_STR='whenever sqlerror exit failure;\n set feedback off;\n set head off;\n set pagesize 0;\n '
-GET_STUDIES_SQL="$SQL_BASIC_STR select acc from study where status = 0 AND trunc(updated_date) >= trunc(sysdate-'${NUM_DAYS}');\n exit\n"
+SQL_BASIC_STR="whenever sqlerror exit failure;\n set feedback off;\n set head off;\n set pagesize 0;\n "
+GET_STUDIES_SQL="${SQL_BASIC_STR} select acc from study where status = 0 AND trunc(updated_date) >= trunc(sysdate-'${NUM_DAYS}');\n exit\n"
 
 Info ------------------------------------------------------------------------------------------ 
 Info Settings:
@@ -37,7 +46,6 @@ Info '  MetaboLights Database connection:  '$DB_CONNECTION
 Info ------------------------------------------------------------------------------------------
 Info '  MetaboLights PRIVATE ftp location: '$PRIV_FTP
 Info '  MetaboLights PUBLIC ftp location : '$PUB_FTP
-Info '  MetaboLights SQL script location : '$SQL_DIRECTORY
 Info ------------------------------------------------------------------------------------------
 
 Info "Testing required parameters"
@@ -59,14 +67,13 @@ for study in $ALL_TAP_TABLES
 
 
 PUBLIC_STUDIES="`echo $GET_STUDIES_SQL | sqlplus -s $DB_CONNECTION`"
-Info "Public Studies found " $PUBLIC_STUDIES
-Info "SQL " $GET_STUDIES_SQL
-Info "SQL2 " $SQL_BASIC_STR
+Info "Public Studies found ${PUBLIC_STUDIES}"
+Info "SQL ${GET_STUDIES_SQL}"
  
 for studies in $PUBLIC_STUDIES
 do
     ls -Fla $studies
-    Info "Study " $studies
+    Info "Study ${studies}"
 done
 
 #mailx -s 'MetaboLights Public File Maintenance' ${EMAILTO} < ${SHELL_LOG_FILE}
