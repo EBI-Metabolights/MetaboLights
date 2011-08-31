b82f0a6dd8364bf7079382175f4cd34764adb0cd
khaug <haug.kenneth@gmail.com>
2011-08-31 11:27:29 +0000

    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@779 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/scripts/maintainPublic.sh
+++ metabolights-webapps/src/main/scripts/maintainPublic.sh
@@ -5,21 +5,27 @@ clear
 . /homes/mtbl/metabolights/metabolights-webapps/src/main/scripts/logging_functions
 source /homes/oracle/ora11setup.sh
 
-#Changable Settings
+#################################
+#  Configurable Options Follow  #
+#################################
+
 EMAILTO=kenneth@ebi.ac.uk
 PROPS_FILE=/homes/mtbl/metabolights/metabolights-webapps/src/main/webapp/resources/application.properties
 SQL_DIRECTORY=/homes/mtbl/metabolights/metabolights-webapps/src/main/sql/
 NUM_DAYS=5
 
-# Do not change any settings below
-SHELL_LOG_FILE=/homes/mtbl/maintainPublic.`date +"%Y-%m-%d %H:%M:%S"`.log3
+#################################
+#  End of Configurable Options  #
+#################################
+
+SHELL_LOG_FILE=/homes/mtbl/maintainPublic.log.`date +"%Y-%m-%d %H:%M:%S"`
 #MAILTO=`grep mtblAdminEmailAddress ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
 DB_CONNECTION=`grep jdbc.username ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
 DB_CONNECTION=$DB_CONNECTION'/'`grep jdbc.password ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
 DB_CONNECTION=$DB_CONNECTION'@'`grep jdbc.databaseurl ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f6 -d:`  
 PUB_FTP=`grep publicFtpLocation ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
 PRIV_FTP=`grep privateFtpLocation ${PROPS_FILE} | grep -v '!' | grep -v '#' |cut -f2 -d=`  
-SQL_BASIC_STR="whenever sqlerror exit failure;\n set feedback off;\n set head off;\n set pagesize 0;\n "
+SQL_BASIC_STR='whenever sqlerror exit failure;\n set feedback off;\n set head off;\n set pagesize 0;\n '
 GET_STUDIES_SQL="$SQL_BASIC_STR select acc from study where status = 0 AND trunc(updated_date) >= trunc(sysdate-'${NUM_DAYS}');\n exit\n"
 
 Info ------------------------------------------------------------------------------------------ 
