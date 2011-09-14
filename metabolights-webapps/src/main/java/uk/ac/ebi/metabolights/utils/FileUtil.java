cb6425dbbb0cb82f66e2a8fe655945d44fed5fe0
khaug <haug.kenneth@gmail.com>
2011-09-14 14:27:24 +0000

    Consolidated all modules into webapp
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@863 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

---
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/utils/FileUtil.java
@@ -0,0 +1,110 @@
+package uk.ac.ebi.metabolights.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileUtil {
+
+	public static void replace (String fileToSearchIn, String textToSearch, String textToReplace) throws IOException{
+		String text;
+		
+		//Get the file into a string
+		text = file2String(fileToSearchIn);
+		
+		//Replace the content
+		text = StringUtils.replace(text, textToSearch, textToReplace);
+		
+		//Save the file
+		String2File(text, fileToSearchIn);
+		
+		
+	}
+	/**
+	 * Returns a string with the contents of a file.
+	 * @param file
+	 * @return
+	 * @throws IOException 
+	 */
+	public static String file2String (String fileToUse) throws IOException{
+		
+		//Instantiate a file object
+		File file = new File (fileToUse);
+		
+		//Use a buffered reader
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = "";
+        String text = "";
+        
+        //Go through the file
+        while((line = reader.readLine()) != null)
+        {
+            //Add the final carriage return and line feed
+        	text += line + "\r\n";
+        }
+        
+        //Close the reader
+        reader.close();
+        
+        //Return the String
+        return text;
+	}
+	/**
+	 * Takes the String passed and saves it in a file 
+	 * @param text: Text to save
+	 * @param fileToSave: File to save (create) with "text" inside.
+	 * @throws IOException 
+	 */
+	public static void String2File (String text, String fileToSave) throws IOException{
+
+		//instantiate a FileWriter
+        FileWriter writer = new FileWriter(fileToSave);
+        
+        //Write the text
+        writer.write(text);
+        
+        //Close the writer
+        writer.close();
+	}
+	/**
+	 * Deletes all files and subdirectories under dir.
+	 * 
+	 * @param dir to delete
+	 * @return Returns true if all deletions were successful.
+	 * If a deletion fails, the method stops attempting to delete and returns false.
+	 */
+	public static boolean deleteDir(File dir) {
+	    if (dir.isDirectory()) {
+	        String[] children = dir.list();
+	        for (int i=0; i<children.length; i++) {
+	            boolean success = deleteDir(new File(dir, children[i]));
+	            if (!success) {
+	                return false;
+	            }
+	        }
+	    }
+
+	    // The directory is now empty so delete it
+	    return dir.delete();
+	}
+	public static boolean fileExists(String path){
+		File file = new File(path);
+
+		return file.exists();
+	}
+	public static boolean fileExists(String path, boolean throwException) throws FileNotFoundException{
+		
+		boolean result = fileExists(path);
+		
+		if (throwException && !result){
+			
+			throw new FileNotFoundException ("Path (" + path + ") not found.");
+		}
+		
+		return result;
+		
+	}
+}
