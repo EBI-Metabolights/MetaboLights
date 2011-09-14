81d0f6c25b5dd8d15a4ce71f6cc69e7a6ddfe408
khaug <haug.kenneth@gmail.com>
2011-09-14 14:19:58 +0000

    Consolidated all modules into webapp
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@857 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- rebuildAll.sh
+++ rebuildAll.sh
@@ -1,8 +1,11 @@
 mvn clean
 rm metabolights-webapps/src/main/webapp/WEB-INF/lib/*.jar
+rm metabolights-webapps/target/metabolights-webapp-*.war
 mvn dependency:copy-dependencies
 mvn compile
 mvn install -DskipTests=true
 cd metabolights-webapps
 mvn war:inplace war:war
+cp /Users/kenneth/Development/metabolights/metabolights-webapps/target/metabolights-webapp-0.1.1-SNAPSHOT.war /nfs/production/panda/metabolights/deploy/metabolights-webapp.war
 cd ..
+ls -Fla /nfs/production/panda/metabolights/deploy/
