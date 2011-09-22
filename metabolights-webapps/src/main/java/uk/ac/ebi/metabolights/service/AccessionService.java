dac8a1fbd51646f0f538a05d82d84b71c1bf7fce
khaug <haug.kenneth@gmail.com>
2011-09-22 15:10:56 +0000

    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@896 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

---
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/service/AccessionService.java
@@ -0,0 +1,27 @@
+/*
+ * EBI MetaboLights Project - 2011.
+ *
+ * File: AccessionService.java
+ *
+ * Modified by:   kenneth
+ *
+ * European Bioinformatics Institute, Wellcome Trust Genome Campus, Hinxton, Cambridgeshire, CB10 1SD, UK.
+ */
+
+package uk.ac.ebi.metabolights.service;
+
+import uk.ac.ebi.metabolights.model.StableId;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kenneth
+ * Date: 20/09/2011
+ * Time: 18:23
+ * To change this template use File | Settings | File Templates.
+ */
+public interface AccessionService {
+
+    public String getAccessionNumber();
+    public void update(StableId stableId);
+
+}
---
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/service/AccessionServiceImpl.java
@@ -0,0 +1,124 @@
+package uk.ac.ebi.metabolights.service;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import uk.ac.ebi.metabolights.dao.StableIdDAO;
+import uk.ac.ebi.metabolights.model.StableId;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kenneth
+ * Date: 20/09/2011
+ * Time: 18:25
+ */
+
+@Service
+public class AccessionServiceImpl implements AccessionService {
+
+    private static Logger logger = Logger.getLogger(AccessionServiceImpl.class);
+
+	@Autowired
+	public StableIdDAO stableIdDAO;
+
+    private StableId stableId;  
+	private String defaultPrefix;
+
+    /**
+	 * Default prefix to use in case of creating the table from scratch
+	 * @return String with the default prefix
+	 */
+	public String getDefaultPrefix (){
+		return defaultPrefix;
+	}
+
+	public void setDefaultPrefix(String defaultPrefix){
+		this.defaultPrefix = defaultPrefix;
+	}
+
+    @Transactional
+    private StableId getStableId() {
+        stableId = stableIdDAO.getStableId();
+        return stableId;
+    }
+
+    @Transactional
+    public void update(StableId stableId) {
+        stableIdDAO.update(stableId);
+    }
+
+	/**
+	 * It has the logic concatenating the Id and the prefix. It also increase the Id by one,
+	 * and invoke persistance of the new value to the database with the new Id increased
+	 * @return String with the concatenated Accession number
+	 */
+	@Transactional
+	public String getAccessionNumber(){
+
+		String accession;
+
+		//Log info
+		logger.info("Getting accession number");
+
+		//Get the stable id object (this is also a static variable, shall we use it instead...)
+		stableId = getNextStableId();
+
+		//Compose the accession number
+		accession = stableId.getPrefix() + stableId.getSeq();
+
+		logger.info("Accession composed is " + accession);
+
+		//Increment the id by one
+		stableId.setSeq(stableId.getSeq() + 1);
+
+		logger.info("persisting incremented accession sequence");
+		//stableIdDAO.update(stableId);
+
+		logger.info("Saved the new stable id "+ stableId.getSeq());
+
+		//Return the accession
+		return accession;
+	}
+
+/**
+	 * Returns the stableId object.
+	 * If the stableId is already instantiated it will return it, avoiding connection to the database (concurrency is not considered).
+	 * If there isn't any it will create a default one using 1 as Id and the defaultPrefix as prefix.
+	 * If there is more than one row an exception will be thrown.
+	 * If there is only one (expected) it will return it.
+	 * @return Unique StableId row in the database
+	 * @throws ExceptionInInitializerError
+	 */
+    @Transactional
+	private StableId getNextStableId() throws ExceptionInInitializerError {
+
+		logger.info("Asking hibernate session for the StableId object.");
+
+		//Get the list of StableId, there should be only one...
+		StableId stableId = getStableId();
+
+		if (stableId == null ) {
+
+			logger.info("There isn't a StableId record. Creating a new default id.");
+
+			//Instantiate the StableId object
+			stableId = new StableId();
+
+			//Populate with default values
+			//Populate de id of the row.
+			stableId.setId(1);
+			stableId.setSeq(1);
+			stableId.setPrefix(defaultPrefix);
+
+			//Persist it
+			//update(stableId);
+
+		}
+
+		//Return stableId instance
+		return stableId;
+	}
+
+
+}
