6033a5d7d165eb7599d998134a17dd69d4156a13
khaug <haug.kenneth@gmail.com>
2011-09-23 13:07:41 +0000

    Consolidated modules and ready for beta layout changes
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@897 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- rebuildAll.sh
+++ rebuildAll.sh
@@ -1,11 +1,12 @@
 mvn clean
 rm metabolights-webapps/src/main/webapp/WEB-INF/lib/*.jar
+rm -r metabolights-webapps/src/main/webapp/WEB-INF/classes
 rm metabolights-webapps/target/metabolights-webapp-*.war
 mvn dependency:copy-dependencies
 mvn compile
 mvn install -DskipTests=true
 cd metabolights-webapps
 mvn war:inplace war:war
-cp /Users/kenneth/Development/metabolights/metabolights-webapps/target/metabolights-webapp-0.1.1-SNAPSHOT.war /nfs/production/panda/metabolights/deploy/metabolights-webapp.war
+cp /Users/kenneth/Development/metabolights/metabolights-webapps/target/metabolights-webapp-0.*-SNAPSHOT.war /nfs/production/panda/metabolights/deploy/metabolights-webapp.war
 cd ..
 ls -Fla /nfs/production/panda/metabolights/deploy/
