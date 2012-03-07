f792b5127cb3838b2cc08eaf452d4120224b790f
pconesa <p.conesa.mingo@gmail.com>
2012-03-07 08:51:42 +0000

    run isacreator.jar in utf-8 mode
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@996 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/ftp_site/submissionTool/plugin/ISAcreator.cmd
+++ metabolights-webapps/src/main/ftp_site/submissionTool/plugin/ISAcreator.cmd
@@ -1,1 +1,10 @@
-java -jar -Dfile.encoding=utf-8 ISAcreator.jar
+
+@ECHO OFF
+ECHO ON
+REM Set JAva virtual machine options
+set JAVA_OPTS=-Dfile.encoding=utf-8 -Xmx1024m -Xms512m
+echo Java options are %JAVA_OPTS% 
+
+java %JAVA_OPTS% -jar ISAcreator-1.5.0.jar
+
+
