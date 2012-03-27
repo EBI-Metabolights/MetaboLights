167bf66fcfc57bf2ac766b7013e83882d589d27e
khaug <haug.kenneth@gmail.com>
2012-03-27 12:40:05 +0000

    Changes to crontab scripts
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1005 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/scripts/tidy.sh
+++ metabolights-webapps/src/main/scripts/tidy.sh
@@ -1,4 +1,4 @@
 # Tidy up old submission files
-find /nfs/production/panda/metabolights/isatab/upload/* -mtime +5 -exec rm -Rf {} \;
-find /nfs/production/panda/metabolights/isatab/metadata/* -mtime +5 -exec rm -Rf {} \;
-find /nfs/production/panda/metabolights/logs/* -mtime +14 -exec rm -Rf {} \;
+find /net/isilonP/public/rw/homes/tc_cm01/upload/* -mtime +5 -exec rm -Rf {} \;
+find /net/isilonP/public/rw/homes/tc_cm01/isatab/metadata/* -mtime +5 -exec rm -Rf {} \;
+find /nfs/production/panda/metabolights/logs/* -mtime +14 -exec rm  {} \;
