43ea675b45e18fe1a11b7aa8a108e8cefa328c03
pconesa <p.conesa.mingo@gmail.com>
2012-04-25 15:21:38 +0000

    Sample approach parallel coordinates
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1019 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

---
+++ metabolights-webapps/src/main/webapp/var/parallel coordinates/PCd3/food-table.js
@@ -0,0 +1,6 @@
+var foods= [
+           {"name":"Male-control","group":"na","gender":"Male","condition":"control","D-alanine":89,"Glucose":0.12, "acetic acid":2.334},
+           {"name":"Male-disease","group":"na","gender":"Female","condition":"control","D-alanine":80,"Glucose":0.16,"acetic acid":2.2},
+           {"name":"Female-control","group":"na","gender":"Male","condition":"disease","D-alanine":56,"Glucose":0.18, "acetic acid":2.6},
+           {"name":"Female-disease","group":"na","gender":"Female","condition":"disease","D-alanine":58,"Glucose":0.13, "acetic acid":2.4}
+           ]
