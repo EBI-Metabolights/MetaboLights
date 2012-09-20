87608df0aa60a8150c09434c48b3ea89ca39b6ee
khaug <haug.kenneth@gmail.com>
2012-09-20 12:14:36 +0000

    updated to handle folders
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1091 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/scripts/move_studies_to_ftp.sh
+++ metabolights-webapps/src/main/scripts/move_studies_to_ftp.sh
@@ -1,27 +1,28 @@
 #!/bin/ksh
 
-#echo "Move file script started" | mailx kenneth@ebi.ac.uk -s "Script /net/isilonP/public/rw/homes/tc_cm01/studies/move_studies_to_ftp.sh started"
-base_dir=/net/isilonP/public/rw/homes/tc_cm01/studies
-private_public="public"
+echo "Move file script started"
+base_dir=/net/isilonP/public/rw/homes/tc_cm01/studies/public
+ftp_folder=/ebi/ftp/pub/databases/metabolights/studies/public/
+fuser_binary=/sbin/fuser
+#fuser_binary=fuser
 
 cd $base_dir
-for priv_pub in $private_public
-do
-    #echo "priv_pub is $priv_pub"
-    if [ -f $priv_pub/*.zip ]
-    then
+echo "Current folder is `pwd`"
 
-      for archive_file in `ls $priv_pub/*.zip`
-      do
-        #echo "archive_file is $archive_file"
-        in_use=$(/sbin/fuser -u $archive_file)
-        if [ ${#in_use} -gt 0 ] ; then
-           echo "$priv_pub file in use $archive_file"
-        else
-           echo "File $archive_file not in use, moving to /ebi/ftp/pub/databases/metabolights/studies/$priv_pub/"
-           mv $archive_file /ebi/ftp/pub/databases/metabolights/studies/$priv_pub/
-        fi
-      done
+if [ -d MTBLS* ]
+then
+  echo "MetaboLights study folder(s) found `ls`" 
 
+  for archive_folder in `ls`
+  do
+    echo "archive_folder is $archive_folder"
+    in_use=$($fuser_binary -u $archive_folder)
+    if [ ${#in_use} -gt 0 ] ; then
+       echo "Folder in use $archive_folder"
+    else
+       echo "Folder $archive_folder not in use, moving to $ftp_folder"
+       mv ./$archive_folder $ftp_folder
     fi
-done
+   done
+
+fi
