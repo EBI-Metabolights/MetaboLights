f5970a99cd03cb21137d1e1d2a9f5491fe00dd33
khaug <haug.kenneth@gmail.com>
2012-11-02 15:58:58 +0000

    Merger of old and new version of SourceForge repository
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1094 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/sql/EmptyDB.oracle.sql
+++ metabolights-webapps/src/main/sql/EmptyDB.oracle.sql
@@ -3,10 +3,10 @@
 declare
 
   cursor c1 is
-    select table_name, constraint_name from user_constraints  where owner = 'ISATAB';
+    select table_name, constraint_name from user_constraints  where owner = 'ISATAB' and table_name not in('DB','REF_ENTRY','REF_XREF','REL_TYPE','REF_METABOLITE');
   
   cursor c2 is
-    select table_name from user_tables;
+    select table_name from user_tables where table_name not in('DB','REF_ENTRY','REF_XREF','REL_TYPE','REF_METABOLITE');
 
 begin
 
@@ -16,10 +16,11 @@ begin
 
  for cur2 in c2 loop
    begin
-	 IF (cur2.table_name <> 'USER_DETAIL' AND cur2.table_name <> 'STABLE_ID') THEN  
+	 IF (cur2.table_name <> 'USER_DETAIL' AND cur2.table_name <> 'STABLE_ID') THEN
      	execute immediate 'ALTER TABLE ISATAB.'||cur2.table_name||' disable all triggers';
      	execute immediate 'TRUNCATE TABLE ISATAB.'||cur2.table_name;
      	execute immediate 'ALTER TABLE ISATAB.'||cur2.table_name||' enable all triggers';
+     	drop sequence ISATAB.USER_DETAIL_SEQ;
      END IF;
    exception when others then null;
   end;
