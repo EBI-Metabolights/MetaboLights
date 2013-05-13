12c2bd8a05b45491c5bcec35d315e39ae9b8e425
pconesa <p.conesa.mingo@gmail.com>
2013-05-13 08:51:09 +0000

    Add ICrossReference class...again! svn and or myself messing things up! ;-)
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1351 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

---
+++ referencelayer/dao/src/main/java/uk/ac/ebi/metabolights/referencelayer/IDAO/ICrossReferenceDAO.java
@@ -0,0 +1,32 @@
+package uk.ac.ebi.metabolights.referencelayer.IDAO;
+
+import uk.ac.ebi.metabolights.referencelayer.domain.CrossReference;
+
+
+/**
+ * Reader for {@link uk.ac.ebi.metabolights.referencelayer.domain.CrossReference} objects from MetaboLights reference layer.
+ * @author Pablo Conesa
+ */
+
+public interface ICrossReferenceDAO {
+
+
+    CrossReference findByCrossReferenceId(Long crossReferenceId) throws DAOException;
+
+
+
+    /**
+     * Updates the CrossReference.
+     * @param crossReference
+     * @throws DAOException
+     */
+    void save(CrossReference crossReference) throws DAOException;
+
+
+    /**
+     * Closes any open system resources allocated to this reader.
+     * @throws DAOException
+     */
+    void close() throws DAOException;
+
+}
