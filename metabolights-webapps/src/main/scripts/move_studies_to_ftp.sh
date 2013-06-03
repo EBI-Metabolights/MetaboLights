4acfcc5d508f0b000db6b5314ba5b5f979ddb74f
khaug <haug.kenneth@gmail.com>
2013-06-03 11:30:40 +0000

    Check if there is a public folder already, if so remove this folder and copy new folder across
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1367 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/scripts/move_studies_to_ftp.sh
+++ metabolights-webapps/src/main/scripts/move_studies_to_ftp.sh
@@ -1,12 +1,15 @@
 #!/bin/ksh
 
 echo "Move file script started"
-base_dir=/nfs/public/rw/homes/tc_cm01/studies/public
-ftp_folder=/ebi/ftp/pub/databases/metabolights/studies/public/
+private_stage=/nfs/public/rw/homes/tc_cm01/studies/public
+public_stage=/nfs/public/rw/homes/tc_cm01/studies/public
+ftp_folder=/ebi/ftp/pub/databases/metabolights/studies/public
 fuser_binary=/sbin/fuser
 #fuser_binary=fuser
 
-cd $base_dir
+
+#find all folders in public stage that should be moved to the public ftp site
+cd $public_stage
 echo "Current folder is `pwd`"
 
 if [ -d MTBLS* ]
@@ -16,13 +19,32 @@ then
   for archive_folder in `ls`
   do
     echo "archive_folder is $archive_folder"
-    in_use=$($fuser_binary -u $archive_folder)
-    if [ ${#in_use} -gt 0 ] ; then
-       echo "Folder in use $archive_folder"
+
+    # check if there is already a folder in the public ftp site, if so we need to remove it first
+    if [ -d $ftp_folder/$archive_folder ] ; then
+       ftp_in_use=$($fuser_binary -u $ftp_folder/$archive_folder)
+       if [ ${#ftp_in_use} -gt 0 ] ; then
+          echo "The public FTP folder is in use $ftp_folder/$archive_folder)"
+       else
+           echo "INFO: there is already a public folder for ${archive_folder} (${ftp_folder}/${archive_folder})"
+           echo "INFO: Removing the folder"
+           rm $ftp_folder/$archive_folder/*
+           rmdir $ftp_folder/$archive_folder
+       fi
+
     else
-       echo "Folder $archive_folder not in use, moving to $ftp_folder"
-       mv ./$archive_folder $ftp_folder
+
+       # try to move the folder
+       in_use=$($fuser_binary -u $archive_folder)
+       if [ ${#in_use} -gt 0 ] ; then
+          echo "Folder in use $archive_folder"
+       else
+          echo "Folder $archive_folder not in use, moving to $ftp_folder"
+          mv ./$archive_folder $ftp_folder/
+       fi
+
     fi
+
    done
 
 fi
