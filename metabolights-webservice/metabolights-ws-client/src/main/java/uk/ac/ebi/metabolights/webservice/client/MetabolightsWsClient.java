e835fd7af4265094ed0f7e847eea8dd3e9c4cd61
pconesa <p.conesa.mingo@gmail.com>
2013-09-02 10:52:33 +0000

    webservice client
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1479 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

---
+++ metabolights-webservice/metabolights-ws-client/src/main/java/uk/ac/ebi/metabolights/webservice/client/MetabolightsWsClient.java
@@ -0,0 +1,89 @@
+package uk.ac.ebi.metabolights.webservice.client;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import uk.ac.ebi.metabolights.repository.model.Study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * User: conesa
+ * Date: 30/08/2013
+ * Time: 12:01
+ */
+public class MetabolightsWsClient {
+
+    private String metabolightsWsUrl = "http://www.ebi.ac.uk/metabolights/webservice/";
+
+    public MetabolightsWsClient(String metabolightsWsUrl){
+        this.metabolightsWsUrl = metabolightsWsUrl;
+    }
+    public MetabolightsWsClient(){};
+
+    private String makeGetRequest(String path) {
+
+        try {
+
+            URL url = new URL(metabolightsWsUrl + path);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String message = org.apache.commons.io.IOUtils.toString(br);
+
+            conn.disconnect();
+
+            return message;
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+    public Study getStudy(String studyIdentifier){
+
+        String path = "study/" + studyIdentifier;
+
+        // Make the request
+        String response = makeGetRequest(path);
+
+        return parseJason(response, Study.class);
+
+    }
+
+    private <T> T parseJason(String response, Class<T> valueType ){
+
+        // Parse response (json) into Study Model...
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            return mapper.readValue(response, valueType);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        return null;
+    }
+
+}
