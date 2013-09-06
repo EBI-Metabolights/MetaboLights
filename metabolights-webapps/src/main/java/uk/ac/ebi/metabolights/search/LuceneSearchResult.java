0fce0bc89f728be81f2f353d47bd201ae50a6830
khaug <haug.kenneth@gmail.com>
2013-09-06 20:02:10 +0000

    Read private study information from the BII lucene index, insert this into the EB-eye search results
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1506 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/search/LuceneSearchResult.java
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/search/LuceneSearchResult.java
@@ -1,3 +1,13 @@
+/*
+ * EBI MetaboLights - http://www.ebi.ac.uk/metabolights
+ * Cheminformatics and Metabolism group
+ *
+ * Last modified: 06/09/13 20:33
+ * Modified by:   kenneth
+ *
+ * Copyright 2013 - European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+ */
+
 package uk.ac.ebi.metabolights.search;
 
 import org.apache.log4j.Logger;
@@ -17,15 +27,18 @@ import java.util.regex.Pattern;
 public class LuceneSearchResult {
 
 	private static Logger logger = Logger.getLogger(LuceneSearchResult.class);
-	
+
 	private List<Assay> assays;
 	private HashMap<String,Set<String>> factors;
 	private HashMap<String,Set<String>> properties;
 	private List<Publication> publications;
-	private List<String> technologies;
+	//private List<String> technologies;
 	private List<String> platforms;
     private List<String> metabolites;
-	private Document doc;
+    private String[] organisms;     //All unique organisms from the document, for EB-eye search results page
+    private String[] technologies;  //All unique technologies from the document, for EB-eye search results page
+    private String[] studyDesign;  //All unique designs from the document, for EB-eye search results page
+    private Document doc;
 	private float score;
 	private Submitter submitter;
 	private boolean isPublic;
@@ -35,28 +48,30 @@ public class LuceneSearchResult {
     private Set<String> affiliations = new LinkedHashSet<String>();
 
     public LuceneSearchResult(Document doc, float score) {
-		this.doc=doc;
-		this.score=score;
-		//Call version 13 this.assays=parseAssays();
+		this.doc = doc;
+		this.score = score;
 		this.assays = parseAssays();
-		
+
 		//Field is called factors in the 1.3 version (not in the enum..yet)
-		this.factors=parseKeyValue("factors");
-		this.properties=parseKeyValue("characteristics");
-		//No longer exist in 1.3 index version:this.technologies=getValues("assay_technology_name");
-		
-		this.publications = parsePublications();
-		
-		this.platforms=getValues("assay_platform");
+		this.factors = parseKeyValue("factors");
+		this.properties = parseKeyValue("characteristics");
+        this.publications = parsePublications();
+        this.platforms = getValues("assay_platform");
         this.metabolites = getValues("Metabolite");
-		
+
+        //Grrr, lots and lots of duplication
+        List<String> uniqueTech = new ArrayList<String>(new HashSet<String>(Arrays.asList(doc.getValues("assay_technology_name"))));
+        this.technologies = uniqueTech.toArray(new String[0]);
+
+        this.studyDesign = getValues("design_value").toArray(new String[0]);
+
+        List<String> uniqueOrganisms = new ArrayList<String>(new HashSet<String>(Arrays.asList(doc.getValues("organism"))));
+        this.organisms = uniqueOrganisms.toArray(new String[0]);
+
 		this.submitter = parseSubmitter();
-		
 		this.isPublic = doc.get("status").equals("PUBLIC");
-		
-			
 		this.releaseDate = parseDate("releaseDate");
-		
+
 		// For weird reason the date in the index is one day less than in the study (file or database)
 		// Let's add, as a quick hack, one day to the public release date
 		// Drawback: queries for public release date in lucene will not be consistent.
@@ -64,13 +79,12 @@ public class LuceneSearchResult {
         cal.setTime(this.releaseDate);
         cal.add(Calendar.DATE, 1);
         this.releaseDate =  cal.getTime();
-		
-		this.submissionDate = parseDate("submissionDate");
 
+		this.submissionDate = parseDate("submissionDate");
         this.contacts = parseContacts();
-		
+
 	}
-	
+
 	public Document getDoc() {
 		return doc;
 	}
@@ -86,7 +100,7 @@ public class LuceneSearchResult {
 	public String getAccStudy() {
 		return doc.get(StudyBrowseField.STUDY_ACC.getName());
 	}
-	
+
 	public String getTitle() {
 		return doc.get(StudyBrowseField.TITLE.getName());
 	}
@@ -94,16 +108,24 @@ public class LuceneSearchResult {
 	public String getOrganism() {
 		return doc.get(StudyBrowseField.ORGANISM.getName());
 	}
-	
+
 	public HashMap<String,Set<String>> getProperties() {
 		return properties;
 	}
-	
-	public List<String> getTechnologies() {
-		return technologies;
-	}
-	
-	public List<String> getPlatforms() {
+
+	//public List<String> getTechnologies() {
+	//	return technologies;
+    //}
+
+    public String[] getTechnologies() {
+        return technologies;
+    }
+
+    public String[] getStudyDesign() {
+        return studyDesign;
+    }
+
+    public List<String> getPlatforms() {
 		return platforms;
 	}
 
@@ -111,7 +133,11 @@ public class LuceneSearchResult {
         return metabolites;
     }
 
-	public HashMap<String,Set<String>> getFactors() {
+    public String[] getOrganisms() {
+        return organisms;
+    }
+
+    public HashMap<String,Set<String>> getFactors() {
 		return factors;
 	}
 
@@ -138,15 +164,15 @@ public class LuceneSearchResult {
 	public String getDescription() {
 		return doc.get("description");
 	}
-	
+
 	public String getDesign() {
 		return doc.get("design_value");
-	}	
-	
+	}
+
 	public void setScore(float score) {
 		this.score = score;
 	}
-	
+
 	public float getScore() {
 		return score;
 	}
@@ -158,7 +184,7 @@ public class LuceneSearchResult {
 	public Date getSubmissionDate(){
 		return this.submissionDate;
 	}
-	
+
 	public List<Publication> getPublications() {
 		return publications;
 	}
@@ -169,7 +195,7 @@ public class LuceneSearchResult {
 	 *
 	 */
 	public class Assay {
-		
+
 		public String getMeasurement() {
 			return measurement;
 		}
@@ -191,7 +217,7 @@ public class LuceneSearchResult {
 		private String measurement;
 		private String technology;
 		private int count;
-		
+
 		public Assay() {
 		}
 
@@ -201,18 +227,18 @@ public class LuceneSearchResult {
 			this.count=count;
 		}
 	}
-	
+
 	/*
 	 * Publication information for a study, for Lucene parsing
 	 * @author kenneth
 	 */
 	public class Publication {
-		
+
 		private String title;
 		private String authors;
 		private String pubmedId;
 		private String doi;
-		
+
 		public String getTitle() {
 			return title;
 		}
@@ -247,7 +273,7 @@ public class LuceneSearchResult {
 
 		public Publication() {
 		}
-		
+
 		public Publication(String title, String authors, String pubmedId, String doi) {
 			this.title = title;
 			this.authors = authors;
@@ -311,84 +337,86 @@ public class LuceneSearchResult {
 
 
     public class Submitter{
-		private String userName, forename, surname, email;
+		private String userName, forename, surname, email, fullName;
 		public Submitter(){};
 		public Submitter(String userName, String forename, String surname, String email){
 			this.userName = userName;
 			this.forename = forename;
-			this.surname= surname;
-			this.email=email;
+			this.surname = surname;
+			this.email = email;
+            this.fullName = forename + " " + surname;
 		}
 
 		public String getName(){ return this.forename;}
 		public String getSurname(){return this.surname;}
 		public String getEmail(){return this.email;}
 		public String getUserName(){return this.userName;}
+        public String getFullName(){return this.fullName;}
 	}
-	
 
-	
+
+
 	/*Parses the user field in the lucene index (there must be only one)
 	 * Sample:  username:conesa|forename:Pablo|surname:Conesa|email:conesa@ebi.ac.uk
 	 */
 	private Submitter parseSubmitter(){
-		
+
 		//Get the user field from the lucene index
 		String submitterString = this.doc.get("user");
-		
+
 		//If null
 		if (submitterString == null) {return new Submitter();}
-		
+
 		//System.out.println("Parsing " + submitterString);
 		//Remove "captions:"
 		submitterString= submitterString.replace("username:", "").replaceAll("forename:", "").replace("surname:","").replaceAll("email:", "");
-		
+
 		//Now we should have something like this: conesa|Pablo|Conesa|conesa@ebi.ac.uk
 		//Attention: the split parameter is a regular expression. As it, the pipe means null and therefore it split one character by one
 		//http://hoskinator.blogspot.com/2006/11/trouble-using-pipe-with-stringsplit.html
 		String[] values = submitterString.split("\\|");
-		
+
 		//Create submitter...
 		return new Submitter(values[0],values[1],values[2],values[3]);
-		
-		
+
+
 	}
 	// Parses a date field inside a lucene index. An example is: 20041011230000000
 	private Date parseDate(String dateField) {
-		
-				
+
+
 		// Get the formatter
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
-		
+
 		//Get the value of the field
 		String date = doc.get(dateField);
-		
+
 		try {
-		
+
 			//If exists...
 			if (date != null){
-			
+
 				//Get only the first 8 characters of the string
 				date = date.substring(0, 8);
 
 				return formatter.parse(date);
 
 			}else{
-				
+
 				// It may be null when the field is not present in the index
 				return null;
 			}
-			
+
 		} catch (Exception e) {
 			//This should never happen
 			logger.info("Field " + dateField + " with value " + date + " can not be parsed into a date.");
 			return null;
 		}
-		
-		
-		
+
+
+
 	}
-	
+
 	private List<Publication> parsePublications() {
 
 		List<Publication> pubList = new ArrayList<Publication>();
@@ -398,7 +426,7 @@ public class LuceneSearchResult {
 		String publicationString = doc.get("publication");  //TODO, can have more than one publication
 		if (publicationString==null)
 			return pubList;
-		
+
 		publicationString = publicationString.replaceAll("title:", "").replaceAll("authors:", "").replaceAll("pubmed:", "").replaceAll("doi:doi:", "doi:");
 		// Lucene field publication contains "title:zxc:as:vxzvc|authors:lots, of, authors|pubmed:123123|doi:doi:123.1232/asdf"
 
@@ -407,37 +435,37 @@ public class LuceneSearchResult {
 		while (tokzr.hasMoreElements()) {
 			String token = (String) tokzr.nextElement();
 			switch (idx) {
-				case 0: pubTitle = token; break; 	
-				case 1: pubAuthors = token; break; 
-				case 2: pubmedId = token; break; 	
-				case 3:	doi = token; break; 		
+				case 0: pubTitle = token; break;
+				case 1: pubAuthors = token; break;
+				case 2: pubmedId = token; break;
+				case 3:	doi = token; break;
 			}
 			idx++;
 		}
-		
+
 		Publication publication = new Publication(pubTitle, pubAuthors, pubmedId, doi);
 		pubList.add(publication);
 
 		return pubList;
 
 	}
-	
-	
-	
+
+
+
 	/**
-	 * Why is a Set necessary? Because there are duplicates in the result. Like the same factor over and over.. 
+	 * Why is a Set necessary? Because there are duplicates in the result. Like the same factor over and over..
 	 * @param fieldName
 	 * @return
 	 */
 	private List<String> getValues (String fieldName) {
-		SortedSet<String> set = new TreeSet<String>(); 
+		SortedSet<String> set = new TreeSet<String>();
 		String[] values = doc.getValues(fieldName);
 		for (String val :values) {
-			
+
 			set.add(val);
 		}
 		return Arrays.asList(set.toArray(new String[set.size()]));
-		
+
 	}
 
 	/**
@@ -449,21 +477,21 @@ public class LuceneSearchResult {
 		List<Assay> assays = new ArrayList<Assay>();
 		String[] assayStrings = doc.getValues(StudyBrowseField.ASSAY_INFO.getName());
 		for (String assayString :assayStrings) {
-			
-			//CREATE THE PATTERN			
-            Pattern pattern = Pattern.compile("assay\\(([^\\|]*)\\|([^\\|]*)\\|(\\d*)\\):"); 
 
-            Matcher matcher = 
+			//CREATE THE PATTERN
+            Pattern pattern = Pattern.compile("assay\\(([^\\|]*)\\|([^\\|]*)\\|(\\d*)\\):");
+
+            Matcher matcher =
             pattern.matcher(assayString);
 
             //Find groups...
-            matcher.find(); 
-            
+            matcher.find();
+
             //Get group 1,2,3
 			String measurement=matcher.group(1);
 			String technology=matcher.group(2);
 			int count=Integer.parseInt(matcher.group(3));
-			
+
 			Assay assay = new Assay(measurement,technology,count);
 			assays.add(assay);
 		}
@@ -494,7 +522,7 @@ public class LuceneSearchResult {
 
 			//If there is a key already
 			if (keyValue.containsKey(key)){
-				
+
 				//Concatenate the value
 				keyValue.get(key).add(value);
 			}else{
@@ -503,15 +531,15 @@ public class LuceneSearchResult {
 				newSet.add(value);
 				keyValue.put(key, newSet);
 			}
-			
+
 		}
 		return keyValue;
 
-		
+
 	}
-	
+
 	private String lineSep = System.getProperty("line.separator");
-	
+
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Score: "+getScore()+lineSep);
@@ -519,7 +547,7 @@ public class LuceneSearchResult {
 		sb.append("Inv: "+ getAccInvestigation()+lineSep);
 		sb.append("Title:"+getTitle()+lineSep);
 		sb.append("Organism:"+getOrganism()+lineSep);
-		
+
 		for (Entry<String,Set<String>> property : getProperties().entrySet()) {
 			sb.append (" -property: "+property.getKey() + ": ");
 			for (String value : property.getValue()){
@@ -527,7 +555,7 @@ public class LuceneSearchResult {
 			}
 			sb.append (lineSep);
 		}
-		
+
 		for (Entry<String,Set<String>> factor : getFactors().entrySet()) {
 			sb.append (" -factor: "+factor.getKey() + ": ");
 			for (String value : factor.getValue()){
