2f2fd4d2c18ea0d9e62d2df8bb6985a34830436c
pconesa <p.conesa.mingo@gmail.com>
2013-09-09 09:43:55 +0000

    delete initial specview project
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1510 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-specview/src/main/java/uk/ac/ebi/metabolights/client/GreetingServiceAsync.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ebi.metabolights.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-public interface GreetingServiceAsync {
-    void greetServer(String name, AsyncCallback<String> async);
-}
