6aed4069aeb8b3ad0403f7a5f536372af3a90ff5
khaug <haug.kenneth@gmail.com>
2013-12-10 12:10:07 +0000

    Done some changes to the downloads page, just to make is easier for new users to download our zip file
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1637 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/ebEyeHelpController.java
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/ebEyeHelpController.java
@@ -1,3 +1,13 @@
+/*
+ * EBI MetaboLights - http://www.ebi.ac.uk/metabolights
+ * Cheminformatics and Metabolism group
+ *
+ * Last modified: 10/12/13 12:09
+ * Modified by:   kenneth
+ *
+ * Copyright 2013 - European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+ */
+
 package uk.ac.ebi.metabolights.controller;
 
 
@@ -17,23 +27,23 @@ import java.net.URL;
 
 
 /**
- * Controller for login and related actions.  
+ * Controller for login and related actions.
  * @author Tejasvi Mahendraker
  *
  */
 @Controller
 public class ebEyeHelpController extends AbstractController{
-	
+
 	 private EBISearchService ebiSearchService;
 
     private @Value("#{ebiServiceURL}") String jndiUrl;
-	           
-	
+
+
 	@RequestMapping({"/ebeyehelp"})
 	public ModelAndView ebeyeHelp(@RequestParam(required=false,value="domain") String domain,
 								  @RequestParam(required=false,value="query") String query,
 								  @RequestParam(required=false,value="url") String url) {
-		
+
 		if (url == null)
             url = jndiUrl;
 
@@ -42,7 +52,7 @@ public class ebEyeHelpController extends AbstractController{
 
 			ebiSearchService =  new EBISearchService_Service().getEBISearchServiceHttpPort();
 		}else{
-		
+
 			//@RequestParam(required=false,value="wsdlurl") String url
 			try {
 				ebiSearchService=  new EBISearchService_Service(new URL(url)).getEBISearchServiceHttpPort();
@@ -51,8 +61,8 @@ public class ebEyeHelpController extends AbstractController{
 				e.printStackTrace();
 			}
 		}
-		
-		
+
+
 		ModelAndView mav = AppContext.getMAVFactory().getFrontierMav ("ebeyehelp"); //name of jsp page must be same as this
 		mav.addObject("query", query);
 		mav.addObject("domain", domain);
@@ -60,34 +70,34 @@ public class ebEyeHelpController extends AbstractController{
 		getSearchResults(mav, domain, query);
 		return mav;
     }
-	
-	
+
+
 	private void getSearchResults(ModelAndView mav, String domain, String query){
-		  
-		  
+
+
 		  ArrayOfString listdomains1 = ebiSearchService.listDomains();
 		  mav.addObject("listDomains", listdomains1.getString());
-		  
+
 		  if (domain != null){
-			  
+
 			  ArrayOfString listFields1 = ebiSearchService.listFields(domain); //lists the fields in the specified domain
 			  //listFields1.getString().add("TAXONOMY");
 			  //listFields1.getString().add("CHEBI");
 			  mav.addObject("listFields", listFields1.getString());
-			  
+
 			  int numOfResults = ebiSearchService.getNumberOfResults(domain, query);
 			  mav.addObject("totalNumberCompounds", numOfResults);
-			  
+
 			  if(numOfResults != 0){
 				  ArrayOfString resultsIds = ebiSearchService.getResultsIds(domain, query, 0, 20);
 				  mav.addObject("resultIds", resultsIds.getString());
-				  
+
 				  ArrayOfArrayOfString arrayOfArrayOfEntries = ebiSearchService.getEntries(domain, resultsIds, listFields1);
 				  mav.addObject("getEntries", arrayOfArrayOfEntries.getArrayOfString());
-				  
+
 				  ArrayOfString ListAddRefFields = ebiSearchService.listAdditionalReferenceFields(domain);
-				  //ArrayOfArrayOfString arrayOfarrayOfEntries = ebiSearchService.getEntries(domain, resultsIds, ListAddRefFields); 
-				  
+				  //ArrayOfArrayOfString arrayOfarrayOfEntries = ebiSearchService.getEntries(domain, resultsIds, ListAddRefFields);
+
 				  mav.addObject("ListAddRefFields", ListAddRefFields.getString());
 				  //mav.addObject("arrayOfarrayOfEntries", arrayOfarrayOfEntries.getArrayOfString());
 			  }
--- metabolights-webapps/src/main/webapp/WEB-INF/jsp/download.jsp
+++ metabolights-webapps/src/main/webapp/WEB-INF/jsp/download.jsp
@@ -1,5 +1,15 @@
 <%@taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 
+<%--
+  ~ EBI MetaboLights - http://www.ebi.ac.uk/metabolights
+  ~ Cheminformatics and Metabolism group
+  ~
+  ~ Last modified: 10/12/13 11:52
+  ~ Modified by:   kenneth
+  ~
+  ~ Copyright 2013 - European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+  --%>
+
 <h2><spring:message code="menu.downloadHelp" /></h2>
 
 
@@ -8,7 +18,7 @@
 <spring:message code="msg.metabolightsAbout12" />
 <p>
 <p>
-<spring:message code="msg.metabolightsAbout9" />
+<%--<spring:message code="msg.metabolightsAbout9" />--%>
 </p>
 
 <p>
@@ -17,6 +27,8 @@
 </p>
 
 <p><spring:message code="msg.metabolightsAbout13" /></p>
+
+<p><spring:message code="msg.metabolightsAbout8" /></p>
 
 <a class="noLine" href="<spring:message code="url.isatools"/>"><img src="img/softwaresuitelogo2.png" alt="ISAtools"/></a>
 <p><spring:message code="msg.metabolightsAbout6" /></p>
