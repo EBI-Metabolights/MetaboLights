fa47b18c5bd2676d07fb691abbea8ec0f138dbd1
khaug <haug.kenneth@gmail.com>
2014-12-08 12:39:20 +0000

    - Fix: metabolites separated with "|" was not split when displaying mzTAB file (MAF)
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1806 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-dao/src/main/java/uk/ac/ebi/metabolights/repository/dao/filesystem/MzTabDAO.java
+++ metabolights-dao/src/main/java/uk/ac/ebi/metabolights/repository/dao/filesystem/MzTabDAO.java
@@ -4,6 +4,26 @@
  *
  * European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
  *
+ * Last modified: 2014-Dec-08
+ * Modified by:   kenneth
+ *
+ * Copyright 2014 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+/*
+ * EBI MetaboLights - http://www.ebi.ac.uk/metabolights
+ * Cheminformatics and Metabolism group
+ *
+ * European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+ *
  * Last modified: 10/17/13 2:31 PM
  * Modified by:   conesa
  *
@@ -53,6 +73,59 @@ public class MzTabDAO {
 
     }
 
+    private MetaboliteAssignmentLine setCommonMetaboliteLineValues(CsvReader fileData, Integer maxColumnNumber) throws IOException {
+
+        MetaboliteAssignmentLine assignmentLine = new MetaboliteAssignmentLine();
+
+        // Get the description V1 and V2
+        String metaboliteIdentification = fileData.get(MetaboliteAssignment.fieldNames.metaboliteIdentification.toString());
+        if (metaboliteIdentification == null || metaboliteIdentification.isEmpty())      //Could be V1 of the MAF, then the column name is different
+            metaboliteIdentification = fileData.get(MetaboliteAssignment.fieldNames.description.toString());  //Version 1 name for the column
+        assignmentLine.setMetaboliteIdentification(metaboliteIdentification);
+
+        assignmentLine.setUnitId(fileData.get(MetaboliteAssignment.fieldNames.unitId.toString()));
+        assignmentLine.setChemicalFormula(fileData.get(MetaboliteAssignment.fieldNames.chemicalFormula.toString()));
+        assignmentLine.setSmiles(fileData.get(MetaboliteAssignment.fieldNames.smiles.toString()));
+        assignmentLine.setInchi(fileData.get(MetaboliteAssignment.fieldNames.inchi.toString()));
+        assignmentLine.setChemicalShift(fileData.get(MetaboliteAssignment.fieldNames.chemicalShift.toString()));
+        assignmentLine.setMultiplicity(fileData.get(MetaboliteAssignment.fieldNames.multiplicity.toString()));
+        assignmentLine.setMassToCharge(fileData.get(MetaboliteAssignment.fieldNames.massToCharge.toString()));
+        assignmentLine.setFragmentation(fileData.get(MetaboliteAssignment.fieldNames.fragmentation.toString()));
+        assignmentLine.setModifications(fileData.get(MetaboliteAssignment.fieldNames.modifications.toString()));
+        assignmentLine.setCharge(fileData.get(MetaboliteAssignment.fieldNames.charge.toString()));
+        assignmentLine.setRetentionTime(fileData.get(MetaboliteAssignment.fieldNames.retentionTime.toString()));
+        assignmentLine.setTaxid(fileData.get(MetaboliteAssignment.fieldNames.taxid.toString()));
+        assignmentLine.setSpecies(fileData.get(MetaboliteAssignment.fieldNames.species.toString()));
+        assignmentLine.setDatabase(fileData.get(MetaboliteAssignment.fieldNames.database.toString()));
+        assignmentLine.setDatabaseVersion(fileData.get(MetaboliteAssignment.fieldNames.databaseVersion.toString()));
+        assignmentLine.setReliability(fileData.get(MetaboliteAssignment.fieldNames.reliability.toString()));
+        assignmentLine.setUri(fileData.get(MetaboliteAssignment.fieldNames.uri.toString()));
+        assignmentLine.setSearchEngine(fileData.get(MetaboliteAssignment.fieldNames.searchEngine.toString()));
+        assignmentLine.setSearchEngineScore(fileData.get(MetaboliteAssignment.fieldNames.searchEngineScore.toString()));
+        assignmentLine.setSmallmoleculeAbundanceSub(fileData.get(MetaboliteAssignment.fieldNames.smallmoleculeAbundanceSub.toString()));
+        assignmentLine.setSmallmoleculeAbundanceStdevSub(fileData.get(MetaboliteAssignment.fieldNames.smallmoleculeAbundanceStdevSub.toString()));
+        assignmentLine.setSmallmoleculeAbundanceStdErrorSub(fileData.get(MetaboliteAssignment.fieldNames.smallmoleculeAbundanceStdErrorSub.toString()));
+
+
+        if (maxColumnNumber == -1)
+            maxColumnNumber = getMaxColumnNumber(fileData, MetaboliteAssignment.fieldNames.smallmoleculeAbundanceStdErrorSub.toString(), maxColumnNumber);    //TODO, this assumes the submitter has not reorganised the column ordering
+
+        Collection<SampleMeasurement> sampleMeasurements = new ArrayList<SampleMeasurement>();
+
+        for (Integer colNumber = maxColumnNumber+1; colNumber <= fileData.getColumnCount(); colNumber++){
+            SampleMeasurement sampleMeasurement = new SampleMeasurement();
+
+            sampleMeasurement.setSampleName(fileData.getHeader(colNumber));
+            sampleMeasurement.setValue(fileData.get(colNumber));
+
+            sampleMeasurements.add(sampleMeasurement);
+        }
+
+        assignmentLine.setSampleMeasurements(sampleMeasurements);
+
+        return assignmentLine;
+    }
+
     private Collection<MetaboliteAssignmentLine> convertToMetaboliteAssignmentLines(String fileName){
 
         Integer maxColumnNumber = -1; //We need to know when the known columns end and the sample records start. 0 is the first column number
@@ -60,78 +133,34 @@ public class MzTabDAO {
         Collection<MetaboliteAssignmentLine> metaboliteAssignmentLines = new ArrayList<MetaboliteAssignmentLine>();
 
         try {
-
             fileData = new CsvReader(fileName, '\t');     // Read the tab-separated MAF
             fileData.readHeaders();                       // Read the header records
+
             while (fileData.readRecord()){
-                MetaboliteAssignmentLine assignmentLine = new MetaboliteAssignmentLine();
+
+                // All common lines
+                MetaboliteAssignmentLine assignmentLine = setCommonMetaboliteLineValues(fileData, maxColumnNumber);
 
                 // Get the identifier V1 and V2
                 String databaseIdentifier = fileData.get(MetaboliteAssignment.fieldNames.databaseIdentifier.toString());
                 if (databaseIdentifier == null || databaseIdentifier.isEmpty())      //Could be V1 of the MAF, then the column name is different
                     databaseIdentifier = fileData.get(MetaboliteAssignment.fieldNames.identifier.toString());     //Version 1 name for the column
-                assignmentLine.setDatabaseIdentifier(databaseIdentifier);
 
+                if (databaseIdentifier.contains("|")){   //The submitter has submitted more than one metabolite per row, this is a supported feature of mzTab
 
-                // Get the description V1 and V2
-                String metaboliteIdentification = fileData.get(MetaboliteAssignment.fieldNames.metaboliteIdentification.toString());
-                if (metaboliteIdentification == null || metaboliteIdentification.isEmpty())      //Could be V1 of the MAF, then the column name is different
-                    metaboliteIdentification = fileData.get(MetaboliteAssignment.fieldNames.description.toString());  //Version 1 name for the column
-                assignmentLine.setMetaboliteIdentification(metaboliteIdentification);
-
-                // All other lines should have the same name in V1 and V2
-                assignmentLine.setUnitId(fileData.get(MetaboliteAssignment.fieldNames.unitId.toString()));
-                assignmentLine.setChemicalFormula(fileData.get(MetaboliteAssignment.fieldNames.chemicalFormula.toString()));
-                assignmentLine.setSmiles(fileData.get(MetaboliteAssignment.fieldNames.smiles.toString()));
-                assignmentLine.setInchi(fileData.get(MetaboliteAssignment.fieldNames.inchi.toString()));
-                assignmentLine.setChemicalShift(fileData.get(MetaboliteAssignment.fieldNames.chemicalShift.toString()));
-                assignmentLine.setMultiplicity(fileData.get(MetaboliteAssignment.fieldNames.multiplicity.toString()));
-                assignmentLine.setMassToCharge(fileData.get(MetaboliteAssignment.fieldNames.massToCharge.toString()));
-                assignmentLine.setFragmentation(fileData.get(MetaboliteAssignment.fieldNames.fragmentation.toString()));
-                assignmentLine.setModifications(fileData.get(MetaboliteAssignment.fieldNames.modifications.toString()));
-                assignmentLine.setCharge(fileData.get(MetaboliteAssignment.fieldNames.charge.toString()));
-                assignmentLine.setRetentionTime(fileData.get(MetaboliteAssignment.fieldNames.retentionTime.toString()));
-                assignmentLine.setTaxid(fileData.get(MetaboliteAssignment.fieldNames.taxid.toString()));
-                assignmentLine.setSpecies(fileData.get(MetaboliteAssignment.fieldNames.species.toString()));
-                assignmentLine.setDatabase(fileData.get(MetaboliteAssignment.fieldNames.database.toString()));
-                assignmentLine.setDatabaseVersion(fileData.get(MetaboliteAssignment.fieldNames.databaseVersion.toString()));
-                assignmentLine.setReliability(fileData.get(MetaboliteAssignment.fieldNames.reliability.toString()));
-                assignmentLine.setUri(fileData.get(MetaboliteAssignment.fieldNames.uri.toString()));
-                assignmentLine.setSearchEngine(fileData.get(MetaboliteAssignment.fieldNames.searchEngine.toString()));
-                assignmentLine.setSearchEngineScore(fileData.get(MetaboliteAssignment.fieldNames.searchEngineScore.toString()));
-                assignmentLine.setSmallmoleculeAbundanceSub(fileData.get(MetaboliteAssignment.fieldNames.smallmoleculeAbundanceSub.toString()));
-                assignmentLine.setSmallmoleculeAbundanceStdevSub(fileData.get(MetaboliteAssignment.fieldNames.smallmoleculeAbundanceStdevSub.toString()));
-                assignmentLine.setSmallmoleculeAbundanceStdErrorSub(fileData.get(MetaboliteAssignment.fieldNames.smallmoleculeAbundanceStdErrorSub.toString()));
-
-
-                if (maxColumnNumber == -1)
-                    maxColumnNumber = getMaxColumnNumber(fileData, MetaboliteAssignment.fieldNames.smallmoleculeAbundanceStdErrorSub.toString(), maxColumnNumber);    //TODO, this assumes the submitter has not reorganised the column ordering
-
-                Collection<SampleMeasurement> sampleMeasurements = new ArrayList<SampleMeasurement>();
-
-                for (Integer colNumber = maxColumnNumber+1; colNumber <= fileData.getColumnCount(); colNumber++){
-                    SampleMeasurement sampleMeasurement = new SampleMeasurement();
-
-                    sampleMeasurement.setSampleName(fileData.getHeader(colNumber));
-                    sampleMeasurement.setValue(fileData.get(colNumber));
-
-                    sampleMeasurements.add(sampleMeasurement);
-                }
-
-                assignmentLine.setSampleMeasurements(sampleMeasurements);
-
-                if (assignmentLine.getDatabaseIdentifier().contains("|")){   //The submitter has submitted more than one metabolite per row, this is a supported feature of mzTab
-
-                    //We need to split the metabolite into separate lines.
-                    for (String eachMetabolite : assignmentLine.getDatabaseIdentifier().split("\\|")){
-                        assignmentLine.setDatabaseIdentifier(eachMetabolite); //Update the identified metabolite
-                        metaboliteAssignmentLines.add(assignmentLine); //Add the row with only one metabolite reported
+                    for (String eachMetabolite : databaseIdentifier.split("\\|")){   //We need to split the metabolite into separate lines.
+                        MetaboliteAssignmentLine newAssignmentLine = setCommonMetaboliteLineValues(fileData, maxColumnNumber);
+                        newAssignmentLine.setDatabaseIdentifier(eachMetabolite);    //Update the identified metabolite
+                        metaboliteAssignmentLines.add(newAssignmentLine);   //Add the row with only one metabolite reported
                     }
 
                 } else {
                     //Only one metabolite reported so add a single rows
+                    assignmentLine.setDatabaseIdentifier(databaseIdentifier);
                     metaboliteAssignmentLines.add(assignmentLine);
                 }
+
+
             }
 
             return metaboliteAssignmentLines;
