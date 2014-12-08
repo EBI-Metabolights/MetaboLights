bd8e76b61544eef03dd60c3733491c94887f64c9
khaug <haug.kenneth@gmail.com>
2014-12-08 11:17:32 +0000

    - Updated pom's to use metabolights-dao to 1.4-SNAPSHOT - Added LipidHome to Analysis page
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1804 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/webapp/WEB-INF/jsp/analysis.jsp
+++ metabolights-webapps/src/main/webapp/WEB-INF/jsp/analysis.jsp
@@ -1,26 +1,37 @@
 <%@taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
 <%--
   ~ EBI MetaboLights - http://www.ebi.ac.uk/metabolights
   ~ Cheminformatics and Metabolism group
   ~
   ~ European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
   ~
-  ~ Last modified: 8/14/14 1:27 PM
-  ~ Modified by:   conesa
+  ~ Last modified: 2014-Dec-05
+  ~ Modified by:   kenneth
   ~
-  ~
-  ~ ©, EMBL, European Bioinformatics Institute, 2014.
+  ~ Copyright 2014 EMBL - European Bioinformatics Institute
   ~
   ~ Licensed under the Apache License, Version 2.0 (the "License");
   ~ you may not use this file except in compliance with the License.
   ~ You may obtain a copy of the License at
   ~
-  ~    http://www.apache.org/licenses/LICENSE-2.0
+  ~      http://www.apache.org/licenses/LICENSE-2.0
   ~
   ~ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
   --%>
 
-Cool stuff coming soon
+<H2>Integrated Analysis Tools</H2>
+
+<P/>
+We are currently developing this section
+<p/>
+
+<H3>LipidHome</H3>
+
+<div class="grid_24">
+  <a href="/metabolights/lipidhome"/>LipidHome</a> is a database of theoretical lipid structures derived from a seed set of lipid sub classes and some lipid chemical space constraints agreed upon with experts in the field. Lipids are organised according to a combination of the LIPID MAPS ontology and the recent publication by the LipidomicNet consortium <a href="http://www.ncbi.nlm.nih.gov/pubmed/23667450"/>(Liebisch G, Vizcaíno JA, Koefeler H, Troztmueller M, Griffiths WJ, Schmitz G, Spener F, Wakelam MJO (2012) Short notation for lipid structures derived from mass spectrometry. J Lipid Res In press.)</a>
+
+</div>
 <%--
     <iframe>/metaboanalyst3</iframe>
   --%>
--- rebuildAll.sh
+++ rebuildAll.sh
@@ -1,20 +1,34 @@
+#!/bin/sh
+export ML="/Users/kenneth/Development/metabolights"
+#export DEPLOY="deploy"
+export DEPLOY=""
+cd $ML
 mvn clean
 rm metabolights-webapps/src/main/webapp/WEB-INF/lib/*.jar
 rm -rf /nfs/public/rw/homes/tc_cm01/dev/upload/process/*
-rm target/dependency/*.jar
-rm metabolights-webapps/target/dependency/*.jar
 rm -r metabolights-webapps/src/main/webapp/WEB-INF/classes
 rm -r /usr/local/tomcat/webapps/metabolights
 rm metabolights-webapps/target/metabolights-webapp-*.war
-mvn dependency:copy-dependencies
-cd metabolights-webservice
-mvn compile package -DskipTests=true
+rm  $ML/metabolights-webservice/target/*.war
+cd metabolights-domain
+mvn clean install $DEPLOY -DskipTests=true
+cd ../metabolights-dao
+mvn clean install $DEPLOY -DskipTests=true
+cd ../metabolights-ws-client
+mvn clean install $DEPLOY -DskipTests=true
+cd ../metabolights-webservice
+mvn clean package $DEPLOY -DskipTests=true
+cd ../metabolights-ws-client
+mvn clean package $DEPLOY -DskipTests=true
 cd ../metabolights-webapps
 mvn compile war:inplace war:war
-cp /Users/kenneth/Development/metabolights/metabolights-webapps/target/metabolights-webapp-*.war /nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-dev.war
+cp $ML/metabolights-webapps/target/metabolights-webapp-*.war /nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-dev.war
+cp $ML/metabolights-webservice/target/metabolights-webservice*.war /nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webservice-dev.war
 cd ..
 ls -Fla /nfs/public/rw/homes/tc_cm01/metabolights/deploy/
 
 echo "to deploy:"
-echo "    scp /nfs/public/rw/homes/tc_cm01/deploy/metabolights-webapp-dev.war ebi-003:/nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-dev.war "
-echo "    scp /nfs/public/rw/homes/tc_cm01/deploy/metabolights-webapp-dev.war ebi-003:/nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-prod.war "
+echo "    scp /nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-dev.war ebi-003:/nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-dev.war "
+echo "    scp /nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-dev.war ebi-003:/nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-prod.war "
+echo "    ------------------- "
+echo "    scp /nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webservice-dev.war ebi-003:/net/isilonP/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webservice-dev.war "
