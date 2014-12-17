cf7095522bd0f28e53eb3e75fdd2d5a8edcad257
khaug <haug.kenneth@gmail.com>
2014-12-17 13:18:24 +0000

    - Problem with "alt" and normal entries  - Added SAB to About page
    git-svn-id: https://svn.code.sf.net/p/metabolomes/code/trunk@1812 71bfadd5-8d58-49b2-b2b0-ff0b8e09f556

--- metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/GenericController.java
+++ metabolights-webapps/src/main/java/uk/ac/ebi/metabolights/controller/GenericController.java
@@ -4,6 +4,26 @@
  *
  * European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
  *
+ * Last modified: 2014-Dec-17
+ * Modified by:   kenneth
+ *
+ * Copyright 2014 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+/*
+ * EBI MetaboLights - http://www.ebi.ac.uk/metabolights
+ * Cheminformatics and Metabolism group
+ *
+ * European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+ *
  * Last modified: 8/14/14 1:27 PM
  * Modified by:   conesa
  *
@@ -48,7 +68,7 @@ public class GenericController {
 	 * @param request
 	 * @return String indicating JSP target
 	 */
-	@RequestMapping(value={ "/about","/help","/download", "/useroptions", "/pleasewait" ,"/analysis"})
+	@RequestMapping(value={ "/about","/help","/download", "/useroptions", "/pleasewait" ,"/analysis", "/advisoryboard"})
 	public ModelAndView modelAndView (HttpServletRequest request) {
 		return lastPartOfUrl(request);
 	}
--- metabolights-webapps/src/main/webapp/WEB-INF/jsp/about.jsp
+++ metabolights-webapps/src/main/webapp/WEB-INF/jsp/about.jsp
@@ -6,6 +6,26 @@
   ~
   ~ European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
   ~
+  ~ Last modified: 2014-Dec-16
+  ~ Modified by:   kenneth
+  ~
+  ~ Copyright 2014 EMBL - European Bioinformatics Institute
+  ~
+  ~ Licensed under the Apache License, Version 2.0 (the "License");
+  ~ you may not use this file except in compliance with the License.
+  ~ You may obtain a copy of the License at
+  ~
+  ~      http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+  --%>
+
+    <%--
+  ~ EBI MetaboLights - http://www.ebi.ac.uk/metabolights
+  ~ Cheminformatics and Metabolism group
+  ~
+  ~ European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+  ~
   ~ Last modified: 6/17/14 3:23 PM
   ~ Modified by:   conesa
   ~
@@ -43,6 +63,8 @@
     </p>
 
    	<p><a href="<spring:url value="statistics"/>"><strong><spring:message code="label.stats"/></strong></a></p>
+
+    <p><a href="<spring:url value="advisoryboard"/>"><strong><spring:message code="label.sab"/></strong></a></p>
 
     <p><a href="<spring:url value="acknowledgements"/>"><strong>Acknowledgements</strong></a></p>
 
