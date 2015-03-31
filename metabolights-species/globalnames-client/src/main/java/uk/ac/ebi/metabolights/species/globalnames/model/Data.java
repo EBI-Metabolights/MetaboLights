f5c35495ee14d0a6d908e7e3a39f4afaa6ee0acf
Kenneth Haug <khaug@yahoo.com>
2015-03-31 14:38:48 +0100

    - Updated species importer

--- metabolights-species/globalnames-client/src/main/java/uk/ac/ebi/metabolights/species/globalnames/model/Data.java
+++ metabolights-species/globalnames-client/src/main/java/uk/ac/ebi/metabolights/species/globalnames/model/Data.java
@@ -4,6 +4,26 @@
  *
  * European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
  *
+ * Last modified: 2015-Mar-31
+ * Modified by:   kenneth
+ *
+ * Copyright 2015 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+/*
+ * EBI MetaboLights - http://www.ebi.ac.uk/metabolights
+ * Cheminformatics and Metabolism group
+ *
+ * European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+ *
  * Last modified: 11/28/13 5:54 PM
  * Modified by:   conesa
  *
@@ -31,6 +51,7 @@ import java.util.Collection;
 public class Data {
 
 	String supplied_name_string;
+	String is_known_name;
 	Collection<Result> results;
 
 	public String getSupplied_name_string() {
@@ -39,6 +60,14 @@ public class Data {
 
 	public void setSupplied_name_string(String supplied_name_string) {
 		this.supplied_name_string = supplied_name_string;
+	}
+
+	public String getIs_known_name() {
+		return is_known_name;
+	}
+
+	public void setIs_known_name(String is_known_name) {
+		this.is_known_name = is_known_name;
 	}
 
 	public Collection<Result> getResults() {
--- metabolights-species/globalnames-client/src/main/java/uk/ac/ebi/metabolights/species/globalnames/model/Result.java
+++ metabolights-species/globalnames-client/src/main/java/uk/ac/ebi/metabolights/species/globalnames/model/Result.java
@@ -4,6 +4,26 @@
  *
  * European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
  *
+ * Last modified: 2015-Mar-31
+ * Modified by:   kenneth
+ *
+ * Copyright 2015 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+/*
+ * EBI MetaboLights - http://www.ebi.ac.uk/metabolights
+ * Cheminformatics and Metabolism group
+ *
+ * European Bioinformatics Institute (EMBL-EBI), European Molecular Biology Laboratory, Wellcome Trust Genome Campus, Hinxton, Cambridge CB10 1SD, United Kingdom
+ *
  * Last modified: 11/28/13 6:04 PM
  * Modified by:   conesa
  *
@@ -44,6 +64,7 @@ public class Result {
 	int match_type;
 	String prescore;
 	float score;
+	String edit_distance;
 
 	public int getData_source_id() {
 		return data_source_id;
@@ -179,5 +200,13 @@ public class Result {
 
 	public void setScore(float score) {
 		this.score = score;
+	}
+
+	public String getEdit_distance() {
+		return edit_distance;
+	}
+
+	public void setEdit_distance(String edit_distance) {
+		this.edit_distance = edit_distance;
 	}
 }
