a1372181ed00bf29e182b12a99eadcad67552455
Kenneth Haug <khaug@yahoo.com>
2015-06-17 14:30:24 +0100

    Changed the email and tooltip text when a study enters review & public status

--- metabolights-domain/src/main/java/uk/ac/ebi/metabolights/repository/model/LiteStudy.java
+++ metabolights-domain/src/main/java/uk/ac/ebi/metabolights/repository/model/LiteStudy.java
@@ -43,7 +43,7 @@ public class LiteStudy extends LiteEntity {
 
 	public enum StudyStatus{
 		SUBMITTED("Submitted", "The study is in progress and is private to you. You can make any changes, even delete the study. This submission will have to pass our initial validation checks."),
-		INCURATION("In curation", "At this stage you can not make any changes. Be advised that further information may be required, and we may contact you to obtain this"),
+		INCURATION("In curation", "At this stage you cannot make any changes. Be advised that further information may be required, and we may contact you to obtain this"),
 		INREVIEW("In review", "Approved by our curation team. Ready to be shared with journals and reviewers."),
 		PUBLIC("Public", "Anyone can view and download it. It is searchable and will be exported to other search engines like http://metabolomexchange.org/");
 		private final String descriptiveName;
