6046e6306cda0e72cfeab33ae4a9e6d01228bc9a
Kalai <kalai@hx-ra-18-1.ebi.ac.uk>
2015-10-13 17:47:10 +0100

    Optional sample collection protocol Validation, when Samples data is provided

--- metabolights-dao/src/main/java/uk/ac/ebi/metabolights/repository/utils/validation/DescriptionConstants.java
+++ metabolights-dao/src/main/java/uk/ac/ebi/metabolights/repository/utils/validation/DescriptionConstants.java
@@ -31,6 +31,7 @@ public class DescriptionConstants {
     public static final String STUDY_FACTORS = "Study factors used in the experiment";
     public static final String PROTOCOLS_ALL = "Comprehensive Experimental protocol";
     public static final String PROTOCOLS_MINIMUM = "Minimum Experimental protocol";
+    public static final String PROTOCOLS_SAMPLE_COLLECTION = "Sample Collection protocol";
 
 
     public static final String ASSAYS = "Assay(s)";
