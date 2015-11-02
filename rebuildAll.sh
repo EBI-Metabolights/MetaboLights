ee770bb51854c37e031abe0b74f80574e4de1dcd
Kenneth Haug <khaug@yahoo.com>
2015-11-02 12:07:40 +0000

    Updated rebuild script to be more flexible

--- rebuildAll.sh
+++ rebuildAll.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
-#export DEPLOY="deploy"
-export DEPLOY=""
-#cd $ML
+export ML="/Users/kenneth/Development/metabolights"
+#export DEPLOY="install -DskipTests=true"
+export DEPLOY=" -DskipTests=true"
+cd $ML
 mvn clean
 rm metabolights-webapps/src/main/webapp/WEB-INF/lib/*.jar
 rm -rf /nfs/public/rw/homes/tc_cm01/dev/upload/process/*
 rm -r metabolights-webapps/src/main/webapp/WEB-INF/classes
 rm -r /usr/local/tomcat/webapps/metabolights
-rm -rf metabolights-webapps/target/*
-rm -rf metabolights-webservice/target/*
+rm metabolights-webapps/target/metabolights-webapp-*.war
+rm  $ML/metabolights-webservice/target/*.war
+rm  -rf $ML/metabolights-webservice/target/*
 cd metabolights-domain
-mvn clean install $DEPLOY -DskipTests=true
+mvn clean install $DEPLOY
 cd ../metabolights-dao
-mvn clean install $DEPLOY -DskipTests=true
-cd ../metabolights-referencelayer/dao
-mvn clean install $DEPLOY -DskipTests=true
-cd ../../MetaboLights-Search
-mvn clean install $DEPLOY -DskipTests=true
+mvn clean install $DEPLOY
+cd ../MetaboLights-Search
+mvn clean package $DEPLOY
 cd ../metabolights-webservice
-mvn clean war:inplace package $DEPLOY -DskipTests=true
+mvn clean package $DEPLOY
 cd ../metabolights-ws-client
-mvn clean install $DEPLOY -DskipTests=true
+mvn clean install $DEPLOY
 cd ../metabolights-webapps
 mvn compile war:inplace war:war
+cp $ML/metabolights-webapps/target/metabolights-webapp-*.war /nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webapp-dev.war
+cp $ML/metabolights-webservice/target/metabolights-webservice*.war /nfs/public/rw/homes/tc_cm01/metabolights/deploy/metabolights-webservice-dev.war
 cd ..
-#ls -Fla /nfs/public/rw/homes/tc_cm01/metabolights/deploy/
+ls -Fla /nfs/public/rw/homes/tc_cm01/metabolights/deploy/
 
 echo "to deploy:"
 echo "    PROD"
