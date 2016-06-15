ff70c7e4aaeec58193e8d005cd377f7819a36fe5
Kenneth Haug <khaug@yahoo.com>
2016-06-15 12:17:17 +0100

    Included more folders to tidy up from cron

--- uncompilables/scripts/tidy.sh
+++ uncompilables/scripts/tidy.sh
@@ -4,5 +4,7 @@ find /nfs/www-prod/web_hx2/cm/metabolights/ml_dev/logs/ -name "*.log" -mtime +14
 find /nfs/www-prod/web_hx2/cm/metabolights/ml_test/logs/ -name "*.log" -mtime +14 -exec rm {} \; 2>&1 >/dev/null
 
 # Tidy up on-demand generated zip files
-find /nfs/www-prod/web_hx2/cm/metabolights/prod/zip_ondemand/ -name "*.zip" -mtime +60 -exec rm {} \; 2>&1 >/dev/null
+find /nfs/www-prod/web_hx2/cm/metabolights/prod/zip_ondemand/ -name "*.zip" -mtime +20 -exec rm {} \; 2>&1 >/dev/null
+find /nfs/metabolights/cheminf/prod/zip_ondemand/ -name "*.zip" -mtime +20 -exec rm {} \; 2>&1 >/dev/null
 find /nfs/www-prod/web_hx2/cm/metabolights/prod/zip_ondemand/ -name "MTBLS*_*_*.zip" -mtime +1 -exec rm {} \; 2>&1 >/dev/null
+find /nfs/metabolights/cheminf/prod/zip_ondemand/ -name "MTBLS*_*_*.zip" -mtime +1 -exec rm {} \; 2>&1 >/dev/null
