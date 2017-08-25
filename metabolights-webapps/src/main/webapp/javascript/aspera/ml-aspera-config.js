c4b6e389c9ae10756200f0c89a100b83f851324b
Venkata Chandrasekhar Nainala <mailcs76@gmail.com>
2017-08-25 14:56:42 +0100

    aspera download error fix

--- metabolights-webapps/src/main/webapp/javascript/aspera/ml-aspera-config.js
+++ metabolights-webapps/src/main/webapp/javascript/aspera/ml-aspera-config.js
@@ -11,13 +11,13 @@ METABOLIGHTS.ConfigFileControl = {
     // remote_upload_password: 'UPLOAD_PASSWORD',
     remote_download_host: 'fasp.ebi.ac.uk',
     remote_download_user: 'fasp-ml',
-    download_token: 'metabolights download',
+    download_token: 'ATM2_ACsXKsKlNtYKyIqlkE3eZMfhrXaAP6Z_Lr4qxOVEMbX-9QAAETLf8GQtfZqCxxJareaMA5_2MTA',
     download_authentication: 'token',
     target_rate_kbps: 500000,
     rate_policy: 'fair',
     cipher: 'none',
     fasp_port: 33001,
-    http_fallback: false,
+    http_fallback: true,
     asperaDownloadConnectPath: 'http://downloads.asperasoft.com/download_connect/',
     // uploadDirectoryPrefix: 'upload'
 };
