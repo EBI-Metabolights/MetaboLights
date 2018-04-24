294a0c721f205dbfbb84ee8cb560731b1aa48a6b
Kenneth Haug <khaug@yahoo.com>
2018-04-24 17:19:16 +0100

    - Updated size report to use bytes not Kilobytes

--- metabolights-domain/src/main/java/uk/ac/ebi/metabolights/utils/json/NumberUtils.java
+++ metabolights-domain/src/main/java/uk/ac/ebi/metabolights/utils/json/NumberUtils.java
@@ -12,22 +12,25 @@ public class NumberUtils {
 
 
 
-    public static String getHumanReadableByteSize(BigDecimal sizeInKiloBytes) {
+    public static String getHumanReadableByteSize(BigDecimal sizeInBytes) {
 
         String humanReadableSize = "";
 
-        double mb = sizeInKiloBytes.doubleValue() / 1024.0;
-        double gb = sizeInKiloBytes.doubleValue() / 1048576.0;
-        double tb = sizeInKiloBytes.doubleValue() / 1073741824.0;
-
+        double kb = sizeInBytes.doubleValue() / 1024.0;
+        double mb = kb / 1024.0;
+        double gb = mb / 1024.0;
+        double tb = gb / 1024.0;
+                                                    
         if (tb > 1) {
             humanReadableSize = formatter.format(tb).concat("TB");
         } else if (gb > 1) {
             humanReadableSize = formatter.format(gb).concat("GB");
         } else if (mb > 1) {
             humanReadableSize = formatter.format(mb).concat("MB");
+        } else if (kb > 1) {
+            humanReadableSize = formatter.format(mb).concat("KB");
         } else {
-            humanReadableSize = formatter.format(sizeInKiloBytes).concat("KB");
+            humanReadableSize = formatter.format(sizeInBytes).concat("Bytes");
         }
         return humanReadableSize;
     }
